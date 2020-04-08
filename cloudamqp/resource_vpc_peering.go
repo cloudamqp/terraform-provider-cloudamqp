3d814ec52f4b4f4cfc9255c0ee19d69e55f22316
Tobias Brodén <tobias@84codes.com>
2020-04-08 15:16:41 +0200

    Extracting the peering request status and adding it to the resource

--- cloudamqp/resource_vpc_peering.go
+++ cloudamqp/resource_vpc_peering.go
@@ -65,10 +65,16 @@ func resourceVpcPeeringRead(d *schema.ResourceData, meta interface{}) error {
 
 	api := meta.(*api.API)
 	log.Printf("[DEBUG] cloudamqp::resource::vpc_peering::read instance id: %v, peering id: %v", d.Get("instance_id"), d.Get("peering_id"))
-	_, err := api.ReadVpcPeeringRequest(d.Get("instance_id").(int), d.Get("peering_id").(string))
+	data, err := api.ReadVpcPeeringRequest(d.Get("instance_id").(int), d.Get("peering_id").(string))
 
 	if err != nil {
 		return err
+	}
+	for k, v := range data {
+		if k == "status" {
+			status := v.(map[string]interface{})
+			d.Set(k, status["code"])
+		}
 	}
 
 	return nil
