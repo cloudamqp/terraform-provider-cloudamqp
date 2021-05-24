43508f9a538e4daed7cb6ec04604e983be5f8bfd
Patrik Ragnarsson <patrik@starkast.net>
2021-05-24 10:24:40 +0200

    Add HTTPS to default firewall settings (#9)
    Users are now able to control if the HTTPS port should be open or not.
    
    The default is that it is open.

--- api/security_firewall.go
+++ api/security_firewall.go
@@ -106,7 +106,7 @@ func (api *API) DeleteFirewallSettings(instanceID int) ([]map[string]interface{}
 
 func DefaultFirewallSettings() map[string]interface{} {
 	defaultRule := map[string]interface{}{
-		"services":    []string{"AMQP", "AMQPS", "STOMP", "STOMPS", "MQTT", "MQTTS"},
+		"services":    []string{"AMQP", "AMQPS", "STOMP", "STOMPS", "MQTT", "MQTTS", "HTTPS"},
 		"ports":       []int{},
 		"ip":          "0.0.0.0/0",
 		"description": "Default",
