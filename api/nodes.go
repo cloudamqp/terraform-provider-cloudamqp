5abeabf3e4907fb859da82b446e8aa5fa7be9896
Patrik Ragnarsson <patrik@starkast.net>
2022-05-25 10:28:43 +0200

    Use the existing instance actions API (#21)
    Uses this https://docs.cloudamqp.com/cloudamqp_api.html#actions
    
    Follow-up to 1d5620ff8551934daaf14f4506150237a1931903

--- api/nodes.go
+++ api/nodes.go
@@ -23,26 +23,35 @@ func (api *API) ReadNodes(instanceID int) ([]map[string]interface{}, error) {
 }
 
 // ReadNode - read out node information of a single node
-func (api *API) ReadNode(instanceID, nodeID int) (map[string]interface{}, error) {
+func (api *API) ReadNode(instanceID int, nodeName string) (map[string]interface{}, error) {
 	data := make(map[string]interface{})
-	failed := make(map[string]interface{})
-	log.Printf("[DEBUG] go-api::nodes::read_node instance id: %d, node id: %d", instanceID, nodeID)
-	path := fmt.Sprintf("api/instances/%d/nodes/%d", instanceID, nodeID)
-	response, err := api.sling.New().Path(path).Receive(&data, &failed)
+	log.Printf("[DEBUG] go-api::nodes::read_node instance id: %d node name: %s", instanceID, nodeName)
+	response, err := api.ReadNodes(instanceID)
 	if err != nil {
 		return nil, err
 	}
-	if response.StatusCode != 200 {
-		return nil, fmt.Errorf("ReadNode failed, status: %v, message: %s", response.StatusCode, failed)
+	for i := range response {
+		if response[i]["name"] == nodeName {
+			data = response[i]
+			break
+		}
 	}
 	return data, nil
 }
 
 // PostAction - request an action for the node (e.g. start/stop/restart RabbitMQ)
-func (api *API) PostAction(instanceID, nodeID int, params map[string]interface{}) (map[string]interface{}, error) {
+func (api *API) PostAction(instanceID int, nodeName string, action string) (map[string]interface{}, error) {
+	var actionAsRoute string
+	params := make(map[string][]string)
 	data := make(map[string]interface{})
 	failed := make(map[string]interface{})
-	path := fmt.Sprintf("api/instances/%d/nodes/%d/action", instanceID, nodeID)
+	if action == "mgmt.restart" {
+		actionAsRoute = "mgmt-restart"
+	} else {
+		actionAsRoute = action
+	}
+	params["nodes"] = append(params["nodes"], nodeName)
+	path := fmt.Sprintf("api/instances/%d/actions/%s", instanceID, actionAsRoute)
 	response, err := api.sling.New().Post(path).BodyJSON(params).Receive(&data, &failed)
 
 	if err != nil {
@@ -52,14 +61,14 @@ func (api *API) PostAction(instanceID, nodeID int, params map[string]interface{}
 		return nil, fmt.Errorf("Action failed, status: %v, message: %s", response.StatusCode, failed)
 	}
 
-	return api.waitOnNodeAction(instanceID, nodeID, params["action"].(string))
+	return api.waitOnNodeAction(instanceID, nodeName, action)
 }
 
-func (api *API) waitOnNodeAction(instanceID, nodeID int, action string) (map[string]interface{}, error) {
+func (api *API) waitOnNodeAction(instanceID int, nodeName string, action string) (map[string]interface{}, error) {
 	log.Printf("[DEBUG] go-api::nodes::waitOnNodeAction waiting")
 	for {
 		time.Sleep(20 * time.Second)
-		data, err := api.ReadNode(instanceID, nodeID)
+		data, err := api.ReadNode(instanceID, nodeName)
 
 		if err != nil {
 			return nil, err
