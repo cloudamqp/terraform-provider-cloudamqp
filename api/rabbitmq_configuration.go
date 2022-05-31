1520791fdf1832e78d839f0337f102fd7df7f3c7
Tobias Brodén <tobias@84codes.com>
2022-05-31 16:05:18 +0200

    Rename rabbitmq configuration functions

--- api/rabbitmq_configuration.go
+++ api/rabbitmq_configuration.go
@@ -7,17 +7,17 @@ import (
 	"time"
 )
 
-func (api *API) ReadRabbitConfiguration(instanceID int) (map[string]interface{}, error) {
+func (api *API) ReadRabbitMqConfiguration(instanceID int) (map[string]interface{}, error) {
 	// Initiale values, 5 attempts and 20 second sleep
-	return api.readRabbitConfigurationWithRetry(instanceID, 5, 20)
+	return api.readRabbitMqConfigurationWithRetry(instanceID, 5, 20)
 }
 
-func (api *API) readRabbitConfigurationWithRetry(instanceID, attempts, sleep int) (map[string]interface{}, error) {
+func (api *API) readRabbitMqConfigurationWithRetry(instanceID, attempts, sleep int) (map[string]interface{}, error) {
 	var data map[string]interface{}
 	failed := make(map[string]interface{})
 	path := fmt.Sprintf("/api/instances/%d/config", instanceID)
 	response, err := api.sling.New().Get(path).Receive(&data, &failed)
-	log.Printf("[DEBUG] go-api::rabbit-configuration#readWithRetry data: %v", data)
+	log.Printf("[DEBUG] go-api::rabbitmq-configuration#readWithRetry data: %v", data)
 
 	if err != nil {
 		return nil, err
@@ -27,10 +27,10 @@ func (api *API) readRabbitConfigurationWithRetry(instanceID, attempts, sleep int
 	case response.StatusCode == 400:
 		if strings.Compare(failed["error"].(string), "Timeout talking to backend") == 0 {
 			if attempts--; attempts > 0 {
-				log.Printf("[INFO] go-api::rabbit-configuration#readWithRetry Timeout talking to backend "+
+				log.Printf("[INFO] go-api::rabbitmq-configuration#readWithRetry Timeout talking to backend "+
 					"attempts left %d and retry in %d seconds", attempts, sleep)
 				time.Sleep(time.Duration(sleep) * time.Second)
-				return api.readRabbitConfigurationWithRetry(instanceID, attempts, 2*sleep)
+				return api.readRabbitMqConfigurationWithRetry(instanceID, attempts, 2*sleep)
 			}
 			return nil, fmt.Errorf("ReadWithRetry failed, status: %v, message: %s", response.StatusCode, failed)
 		}
@@ -38,12 +38,12 @@ func (api *API) readRabbitConfigurationWithRetry(instanceID, attempts, sleep int
 	return data, nil
 }
 
-func (api *API) UpdateRabbitConfiguration(instanceID int, params map[string]interface{}) error {
+func (api *API) UpdateRabbitMqConfiguration(instanceID int, params map[string]interface{}) error {
 	// Initiale values, 5 attempts and 20 second sleep
-	return api.updateRabbitConfigurationWithRetry(instanceID, 5, 20, params)
+	return api.updateRabbitMqConfigurationWithRetry(instanceID, 5, 20, params)
 }
 
-func (api *API) updateRabbitConfigurationWithRetry(instanceID, attempts, sleep int, params map[string]interface{}) error {
+func (api *API) updateRabbitMqConfigurationWithRetry(instanceID, attempts, sleep int, params map[string]interface{}) error {
 	failed := make(map[string]interface{})
 	path := fmt.Sprintf("api/instances/%d/config", instanceID)
 	response, err := api.sling.New().Put(path).BodyJSON(params).Receive(nil, &failed)
@@ -56,10 +56,10 @@ func (api *API) updateRabbitConfigurationWithRetry(instanceID, attempts, sleep i
 	case response.StatusCode == 400:
 		if strings.Compare(failed["error"].(string), "Timeout talking to backend") == 0 {
 			if attempts--; attempts > 0 {
-				log.Printf("[INFO] go-api::rabbit-configuration#updateWithRetry Timeout talking to backend "+
+				log.Printf("[INFO] go-api::rabbitmq-configuration#updateWithRetry Timeout talking to backend "+
 					"attempts left %d and retry in %d seconds", attempts, sleep)
 				time.Sleep(time.Duration(sleep) * time.Second)
-				return api.updateRabbitConfigurationWithRetry(instanceID, attempts, 2*sleep, params)
+				return api.updateRabbitMqConfigurationWithRetry(instanceID, attempts, 2*sleep, params)
 			} else {
 				return fmt.Errorf("UpdateWithRetry failed, status: %v, message: %s", response.StatusCode, failed)
 			}
@@ -68,6 +68,6 @@ func (api *API) updateRabbitConfigurationWithRetry(instanceID, attempts, sleep i
 	return nil
 }
 
-func (api *API) DeleteRabbitConfiguration() error {
+func (api *API) DeleteRabbitMqConfiguration() error {
 	return nil
 }
