bf5addd1e503dda3c85dfe44a744e84f39760af0
Tobias Brodén <tobias@84codes.com>
2022-12-07 13:11:34 +0100

    Update RabbitMQ configuration response handling when backend is busy
    Extend response handling when read or update RabbitMQ configuration
    - Takes care of status code 503
    - Updates retry with configurable sleep and timeout

--- api/rabbitmq_configuration.go
+++ api/rabbitmq_configuration.go
@@ -7,15 +7,16 @@ import (
 	"time"
 )
 
-func (api *API) ReadRabbitMqConfiguration(instanceID int) (map[string]interface{}, error) {
-	// Initiale values, 5 attempts and 20 second sleep
-	return api.readRabbitMqConfigurationWithRetry(instanceID, 5, 20)
+func (api *API) ReadRabbitMqConfiguration(instanceID, sleep, timeout int) (map[string]interface{}, error) {
+	return api.readRabbitMqConfigurationWithRetry(instanceID, 1, sleep, timeout)
 }
 
-func (api *API) readRabbitMqConfigurationWithRetry(instanceID, attempts, sleep int) (map[string]interface{}, error) {
-	var data map[string]interface{}
-	failed := make(map[string]interface{})
-	path := fmt.Sprintf("/api/instances/%d/config", instanceID)
+func (api *API) readRabbitMqConfigurationWithRetry(instanceID, attempt, sleep, timeout int) (map[string]interface{}, error) {
+	var (
+		data   map[string]interface{}
+		failed map[string]interface{}
+		path   = fmt.Sprintf("/api/instances/%d/config", instanceID)
+	)
 	response, err := api.sling.New().Get(path).Receive(&data, &failed)
 	log.Printf("[DEBUG] go-api::rabbitmq-configuration#readWithRetry data: %v", data)
 
@@ -23,49 +24,74 @@ func (api *API) readRabbitMqConfigurationWithRetry(instanceID, attempts, sleep i
 		return nil, err
 	}
 
-	switch {
-	case response.StatusCode == 400:
+	switch response.StatusCode {
+	case 200:
+		return data, nil
+	case 400:
 		if strings.Compare(failed["error"].(string), "Timeout talking to backend") == 0 {
-			if attempts--; attempts > 0 {
-				log.Printf("[INFO] go-api::rabbitmq-configuration#readWithRetry Timeout talking to backend "+
-					"attempts left %d and retry in %d seconds", attempts, sleep)
-				time.Sleep(time.Duration(sleep) * time.Second)
-				return api.readRabbitMqConfigurationWithRetry(instanceID, attempts, 2*sleep)
-			}
-			return nil, fmt.Errorf("ReadWithRetry failed, status: %v, message: %s", response.StatusCode, failed)
+			log.Printf("[DEBUG] go-api::rabbitmq-configuration::readWithRetry Timeout talking to backend, "+
+				"attempt: %d, until timeout: %d", attempt, (timeout - (attempt * sleep)))
+			attempt++
+			time.Sleep(time.Duration(sleep) * time.Second)
+			return api.readRabbitMqConfigurationWithRetry(instanceID, attempt, sleep, timeout)
+		} else {
+			break
+		}
+	case 503:
+		if strings.Compare(failed["error"].(string), "Timeout talking to backend") == 0 {
+			log.Printf("[DEBUG] go-api::rabbitmq-configuration::readWithRetry Timeout talking to backend, "+
+				"attempt: %d, until timeout: %d", attempt, (timeout - (attempt * sleep)))
+			attempt++
+			time.Sleep(time.Duration(sleep) * time.Second)
+			return api.readRabbitMqConfigurationWithRetry(instanceID, attempt, sleep, timeout)
 		}
 	}
-	return data, nil
+	return nil, fmt.Errorf("Read RabbitMQ configuration failed, status: %v, message: %s", response.StatusCode, failed)
 }
 
-func (api *API) UpdateRabbitMqConfiguration(instanceID int, params map[string]interface{}) error {
-	// Initiale values, 5 attempts and 20 second sleep
-	return api.updateRabbitMqConfigurationWithRetry(instanceID, 5, 20, params)
+func (api *API) UpdateRabbitMqConfiguration(instanceID int, params map[string]interface{},
+	sleep, timeout int) error {
+	return api.updateRabbitMqConfigurationWithRetry(instanceID, params, 1, sleep, timeout)
 }
 
-func (api *API) updateRabbitMqConfigurationWithRetry(instanceID, attempts, sleep int, params map[string]interface{}) error {
-	failed := make(map[string]interface{})
-	path := fmt.Sprintf("api/instances/%d/config", instanceID)
+func (api *API) updateRabbitMqConfigurationWithRetry(instanceID int, params map[string]interface{},
+	attempt, sleep, timeout int) error {
+	var (
+		failed map[string]interface{}
+		path   = fmt.Sprintf("api/instances/%d/config", instanceID)
+	)
 	response, err := api.sling.New().Put(path).BodyJSON(params).Receive(nil, &failed)
-
 	if err != nil {
 		return err
+	} else if attempt*sleep > timeout {
+		return fmt.Errorf("Update RabbitMQ configuraiton failed, reached timeout of %d seconds", timeout)
 	}
 
-	switch {
-	case response.StatusCode == 400:
+	switch response.StatusCode {
+	case 200:
+		return nil
+	case 400:
 		if strings.Compare(failed["error"].(string), "Timeout talking to backend") == 0 {
-			if attempts--; attempts > 0 {
-				log.Printf("[INFO] go-api::rabbitmq-configuration#updateWithRetry Timeout talking to backend "+
-					"attempts left %d and retry in %d seconds", attempts, sleep)
-				time.Sleep(time.Duration(sleep) * time.Second)
-				return api.updateRabbitMqConfigurationWithRetry(instanceID, attempts, 2*sleep, params)
-			} else {
-				return fmt.Errorf("UpdateWithRetry failed, status: %v, message: %s", response.StatusCode, failed)
-			}
+			log.Printf("[DEBUG] go-api::rabbitmq-configuration::updateWithRetry Timeout talking to backend, "+
+				"attempt: %d, until timeout: %d", attempt, (timeout - (attempt * sleep)))
+			attempt++
+			time.Sleep(time.Duration(sleep) * time.Second)
+			return api.updateRabbitMqConfigurationWithRetry(instanceID, params, attempt, sleep, timeout)
+		} else {
+			break
+		}
+	case 503:
+		if strings.Compare(failed["error"].(string), "Timeout talking to backend") == 0 {
+			log.Printf("[DEBUG] go-api::rabbitmq-configuration::updateWithRetry Timeout talking to backend, "+
+				"attempt: %d, until timeout: %d", attempt, (timeout - (attempt * sleep)))
+			attempt++
+			time.Sleep(time.Duration(sleep) * time.Second)
+			return api.updateRabbitMqConfigurationWithRetry(instanceID, params, attempt, sleep, timeout)
+		} else {
+			break
 		}
 	}
-	return nil
+	return fmt.Errorf("Update RabbitMQ configuration failed, status: %v, message: %s", response.StatusCode, failed)
 }
 
 func (api *API) DeleteRabbitMqConfiguration() error {
