b243f423b2d317fd7d5c3c4318c345fdb82ae3b2
Tobias Brodén <47317658+tbroden84@users.noreply.github.com>
2023-03-07 09:55:55 +0100

    Add support for AWS EventBridge integration (#192)

---
+++ cloudamqp/resource_cloudamqp_aws_eventbridge.go
@@ -0,0 +1,158 @@
+package cloudamqp
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/84codes/go-api/api"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAwsEventBridge() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAwsEventBridgeCreate,
+		Read:   resourceAwsEventBridgeRead,
+		Delete: resourceAwsEventBridgeDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"instance_id": {
+				Type:        schema.TypeInt,
+				ForceNew:    true,
+				Required:    true,
+				Description: "Instance identifier",
+			},
+			"aws_account_id": {
+				Type:        schema.TypeString,
+				ForceNew:    true,
+				Required:    true,
+				Description: "The 12 digit AWS Account ID where you want the events to be sent to.",
+			},
+			"aws_region": {
+				Type:        schema.TypeString,
+				ForceNew:    true,
+				Required:    true,
+				Description: "The AWS region where you the events to be sent to. (e.g. us-west-1, us-west-2, ..., etc.)",
+			},
+			"vhost": {
+				Type:        schema.TypeString,
+				ForceNew:    true,
+				Required:    true,
+				Description: "The VHost the queue resides in.",
+			},
+			"queue": {
+				Type:        schema.TypeString,
+				ForceNew:    true,
+				Required:    true,
+				Description: "A (durable) queue on your RabbitMQ instance.",
+			},
+			"with_headers": {
+				Type:        schema.TypeBool,
+				ForceNew:    true,
+				Required:    true,
+				Description: "Include message headers in the event data.",
+			},
+			"status": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Always set to null, unless there is an error starting the EventBridge",
+			},
+		},
+	}
+}
+
+func resourceAwsEventBridgeCreate(d *schema.ResourceData, meta interface{}) error {
+	var (
+		api        = meta.(*api.API)
+		keys       = awsEventbridgeAttributeKeys()
+		params     = make(map[string]interface{})
+		instanceID = d.Get("instance_id").(int)
+	)
+
+	for _, k := range keys {
+		if v := d.Get(k); v != nil {
+			params[k] = v
+		}
+	}
+
+	data, err := api.CreateAwsEventBridge(instanceID, params)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(data["id"].(string))
+	return nil
+}
+
+func resourceAwsEventBridgeRead(d *schema.ResourceData, meta interface{}) error {
+	if strings.Contains(d.Id(), ",") {
+		log.Printf("[DEBUG] cloudamqp::resource::aws-eventbridge::read id contains : %v", d.Id())
+		s := strings.Split(d.Id(), ",")
+		log.Printf("[DEBUG] cloudamqp::resource::aws-eventbridge::read split ids: %v, %v", s[0], s[1])
+		d.SetId(s[0])
+		instanceID, _ := strconv.Atoi(s[1])
+		d.Set("instance_id", instanceID)
+	}
+	if d.Get("instance_id").(int) == 0 {
+		return fmt.Errorf("Missing instance identifier: {resource_id},{instance_id}")
+	}
+
+	var (
+		api        = meta.(*api.API)
+		instanceID = d.Get("instance_id").(int)
+	)
+
+	log.Printf("[DEBUG] cloudamqp::resource::aws-eventbridge::read ID: %v, instanceID %v", d.Id(), instanceID)
+	data, err := api.ReadAwsEventBridge(instanceID, d.Id())
+	if err != nil {
+		return err
+	}
+
+	for k, v := range data {
+		if validateAwsEventBridgeSchemaAttribute(k) {
+			if v == nil {
+				continue
+			}
+			if err = d.Set(k, v); err != nil {
+				return fmt.Errorf("error setting %s for resource %s: %s", k, d.Id(), err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func resourceAwsEventBridgeDelete(d *schema.ResourceData, meta interface{}) error {
+	var (
+		api        = meta.(*api.API)
+		instanceID = d.Get("instance_id").(int)
+	)
+
+	return api.DeleteAwsEventBridge(instanceID, d.Id())
+}
+
+func awsEventbridgeAttributeKeys() []string {
+	return []string{
+		"aws_account_id",
+		"aws_region",
+		"vhost",
+		"queue",
+		"with_headers",
+	}
+}
+
+func validateAwsEventBridgeSchemaAttribute(key string) bool {
+	switch key {
+	case "aws_account_id",
+		"aws_region",
+		"vhost",
+		"queue",
+		"with_headers",
+		"status":
+		return true
+	}
+	return false
+}
---
+++ docs/resources/integration_aws_eventbridge.md
@@ -0,0 +1,64 @@
+---
+layout: "cloudamqp"
+page_title: "CloudAMQP: cloudamqp_integration_aws_eventbridge"
+description: |-
+  Creates and manages an AWS EventBridge for a CloudAMQP instance.
+---
+
+# cloudamqp_integration_aws_eventbridge
+
+This resource allows you to create and manage, an [AWS EventBridge](https://aws.amazon.com/eventbridge/) for a CloudAMQP instance. Once created, continue to map the EventBridge in the [AWS Eventbridge console](https://console.aws.amazon.com/events/home).
+
+~>  Our consumer needs to have exclusive usage to the configured queue and the maximum body size allowed on msgs by AWS is 256kb. The message body has to be valid JSON for AWS Eventbridge to accept it. If messages are too large or are not valid JSON, they will be rejected (tip: setup a dead-letter queue to catch them).
+
+Not possible to update this resource. Any changes made to the argument will destroy and recreate the resource. Hence why all arguments use ForceNew.
+
+Only available for dedicated subscription plans.
+
+## Example usage
+
+```hcl
+resource "cloudamqp_instance" "instance" {
+  name   = "Test instance"
+  plan   = "squirrel-1"
+  region = "amazon-web-services::us-west-1"
+  rmq_version = "3.11.5"
+  tags = ["aws"]
+}
+
+resource "cloudamqp_integration_aws_eventbridge" "aws_eventbridge" {
+  instance_id = cloudamqp_instance.instance.id
+  vhost = cloudamqp_instance.instance.vhost
+  queue = "<QUEUE-NAME>"
+  aws_account_id = "<AWS-ACCOUNT-ID>"
+  aws_region = "us-west-1"
+  with_headers = true
+}
+```
+
+## Argument references
+
+The following arguments are supported:
+
+* `aws_account_id` - (ForceNew/Required) The 12 digit AWS Account ID where you want the events to be sent to.
+* `aws_region`- (ForceNew/Required) The AWS region where you the events to be sent to. (e.g. us-west-1, us-west-2, ..., etc.)
+* `vhost`- (ForceNew/Required) The VHost the queue resides in.
+* `queue` - (ForceNew/Required) A (durable) queue on your RabbitMQ instance.
+* `with_headers` - (ForceNew/Required) Include message headers in the event data. `({ "headers": { }, "body": { "your": "message" } })`
+
+## Attributes Reference
+
+All attributes reference are computed
+
+* `id`  - The identifier for this resource.
+* `status` - Always set to null, unless there is an error starting the EventBridge.
+
+## Dependency
+
+This resource depends on CloudAMQP instance identifier, `cloudamqp_instance.instance.id`.
+
+## Import
+
+`cloudamqp_integration_aws_eventbridge` can be imported using CloudAMQP internal identifier of the AWS EventBridge together (CSV separated) with the instance identifier. To retrieve the AWS EventBridge identifier, use [CloudAMQP API](https://docs.cloudamqp.com/cloudamqp_api.html#list-eventbridges)
+
+`terraform import cloudamqp_integration_aws_eventbridge.aws_eventbridge <id>,<instance_id>`
