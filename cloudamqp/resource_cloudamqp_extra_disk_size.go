3f69a2c29fe298b7acf649575aecbd6daff853b4
Tobias Brodén <tobias@84codes.com>
2023-03-17 13:01:56 +0100

    Update to support multiple platforms

--- cloudamqp/resource_cloudamqp_extra_disk_size.go
+++ cloudamqp/resource_cloudamqp_extra_disk_size.go
@@ -1,6 +1,7 @@
 package cloudamqp
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/84codes/go-api/api"
@@ -11,44 +12,124 @@ func resourceExtraDiskSize() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceExtraDiskSizeUpdate,
 		Read:   resourceExtraDiskSizeRead,
+		Update: resourceExtraDiskSizeUpdate,
 		Delete: resourceExtraDiskSizeDelete,
-		Importer: &schema.ResourceImporter{
-			State: schema.ImportStatePassthrough,
-		},
 		Schema: map[string]*schema.Schema{
 			"instance_id": {
 				Type:        schema.TypeInt,
-				ForceNew:    true,
 				Required:    true,
 				Description: "Instance identifier",
 			},
 			"extra_disk_size": {
 				Type:        schema.TypeInt,
-				ForceNew:    true,
 				Required:    true,
 				Description: "Extra disk size in GB",
+			},
+			"allow_downtime": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "When resizing disk, allow downtime to do so",
+			},
+			"sleep": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     30,
+				Description: "Configurable sleep time in seconds between retries for firewall configuration",
+			},
+			"timeout": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     1800,
+				Description: "Configurable timeout time in seconds for firewall configuration",
+			},
+			"nodes": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"disk_size": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"additional_disk_size": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+					},
+				},
 			},
 		},
 	}
 }
 
 func resourceExtraDiskSizeUpdate(d *schema.ResourceData, meta interface{}) error {
-	api := meta.(*api.API)
-	params := make(map[string]interface{})
+	var (
+		api    = meta.(*api.API)
+		params = make(map[string]interface{})
+	)
+
 	params["extra_disk_size"] = d.Get("extra_disk_size")
-	_, err := api.ResizeDisk(d.Get("instance_id").(int), params)
+	params["allow_downtime"] = d.Get("allow_downtime")
+
+	_, err := api.ResizeDisk(d.Get("instance_id").(int), params, 30, 1800)
 	if err != nil {
 		return err
 	}
 	id := strconv.Itoa(d.Get("instance_id").(int))
 	d.SetId(id)
-	return nil
+
+	return resourceExtraDiskSizeRead(d, meta)
 }
 
 func resourceExtraDiskSizeRead(d *schema.ResourceData, meta interface{}) error {
+	var (
+		api        = meta.(*api.API)
+		instanceID = d.Get("instance_id").(int)
+	)
+
+	data, err := api.ReadNodes(instanceID)
+	if err != nil {
+		return err
+	}
+
+	nodes := make([]map[string]interface{}, len(data))
+	for k, v := range data {
+		nodes[k] = readDiskNode(v)
+	}
+
+	if err = d.Set("nodes", nodes); err != nil {
+		return fmt.Errorf("error setting nodes for resource %s, %s", d.Id(), err)
+	}
+
 	return nil
 }
 
 func resourceExtraDiskSizeDelete(d *schema.ResourceData, meta interface{}) error {
+	// Just remove this resource from the state file, no action taken in backend.
 	return nil
+}
+
+func readDiskNode(data map[string]interface{}) map[string]interface{} {
+	node := make(map[string]interface{})
+	for k, v := range data {
+		if validateDiskSchemaAttribute(k) {
+			node[k] = v
+		}
+	}
+	return node
+}
+
+func validateDiskSchemaAttribute(key string) bool {
+	switch key {
+	case "name",
+		"disk_size",
+		"additional_disk_size":
+		return true
+	}
+	return false
 }
