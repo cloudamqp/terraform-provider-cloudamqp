662bacdca07460a7833e4b7965fdf7d71a0b07e9
Tobias Brodén <tobias@84codes.com>
2023-09-07 14:57:12 +0200

    Remove force new from node action

--- cloudamqp/resource_cloudamqp_node_actions.go
+++ cloudamqp/resource_cloudamqp_node_actions.go
@@ -16,7 +16,6 @@ func resourceNodeAction() *schema.Resource {
 			"instance_id": {
 				Type:        schema.TypeInt,
 				Required:    true,
-				ForceNew:    true,
 				Description: "Instance identifier",
 			},
 			"node_name": {
