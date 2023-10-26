1d4011a8c125500da2ccb9d6c798279af9a58c55
Tobias Brodén <47317658+tbroden84@users.noreply.github.com>
2023-10-26 13:07:00 +0200

    Add endpoints to rotate password and API key (#39)
    Endpoints that support to rotate password and API key for an account.

--- api/account.go
+++ api/account.go
@@ -7,9 +7,12 @@ import (
 )
 
 func (api *API) ListInstances() ([]map[string]interface{}, error) {
-	var data []map[string]interface{}
-	failed := make(map[string]interface{})
-	path := "api/instances"
+	var (
+		data   []map[string]interface{}
+		failed map[string]interface{}
+		path   = "api/instances"
+	)
+
 	response, err := api.sling.New().Path(path).Receive(&data, &failed)
 	log.Printf("[DEBUG] go-api::account::list_instances data: %v", data)
 	if err != nil {
@@ -22,9 +25,12 @@ func (api *API) ListInstances() ([]map[string]interface{}, error) {
 }
 
 func (api *API) ListVpcs() ([]map[string]interface{}, error) {
-	var data []map[string]interface{}
-	failed := make(map[string]interface{})
-	path := "/api/vpcs"
+	var (
+		data   []map[string]interface{}
+		failed map[string]interface{}
+		path   = "/api/vpcs"
+	)
+
 	response, err := api.sling.New().Path(path).Receive(&data, &failed)
 	log.Printf("[DEBUG] go-api::vpc::list data: %v", data)
 
@@ -42,4 +48,48 @@ func (api *API) ListVpcs() ([]map[string]interface{}, error) {
 	}
 
 	return data, nil
+}
+
+func (api *API) RotatePassword(instanceID int) error {
+	var (
+		failed map[string]interface{}
+		path   = fmt.Sprintf("api/instances/%d/account/rotate-password", instanceID)
+	)
+
+	response, err := api.sling.New().Post(path).Receive(nil, &failed)
+	if err != nil {
+		return err
+	}
+
+	switch response.StatusCode {
+	case 200:
+		return nil
+	case 204:
+		return nil
+	default:
+		return fmt.Errorf("failed to rotate api key, statusCode: %v, failed: %v",
+			response.StatusCode, failed)
+	}
+}
+
+func (api *API) RotateApiKey(instanceID int) error {
+	var (
+		failed map[string]interface{}
+		path   = fmt.Sprintf("api/instances/%d/account/rotate-apikey", instanceID)
+	)
+
+	response, err := api.sling.New().Post(path).Receive(nil, &failed)
+	if err != nil {
+		return err
+	}
+
+	switch response.StatusCode {
+	case 200:
+		return nil
+	case 204:
+		return nil
+	default:
+		return fmt.Errorf("failed to rotate api key, statusCode: %v, failed: %v",
+			response.StatusCode, failed)
+	}
 }
