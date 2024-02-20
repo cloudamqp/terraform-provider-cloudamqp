e783cc5dc0eaef07dc93bc2ff0eacc8e3bfabeee
Tobias Brodén <tobias@84codes.com>
2024-02-20 16:25:47 +0100

    Clean out test/configurations

--- test/configurations/cloudamqp_instance/main.tf
+++
@@ -1,6 +0,0 @@
-resource "cloudamqp_instance" "instance" {
-	name = "{{or .InstanceName `TestAccInstance`}}"
-	plan = "{{or .InstancePlan `bunny-1`}}"
-	region = "{{or .InstanceRegion `amazon-web-services::us-east-1`}}"
-	tags = ["{{or .InstanceTags `terraform`}}"]
-}
