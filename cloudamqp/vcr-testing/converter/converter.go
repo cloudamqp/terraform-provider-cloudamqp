80e51b19ef994f575717b670e0c3835be418950a
Tobias Brodén <tobias@84codes.com>
2024-03-01 13:46:04 +0100

    Update how to convert string array

--- cloudamqp/vcr-testing/converter/converter.go
+++ cloudamqp/vcr-testing/converter/converter.go
@@ -2,17 +2,13 @@ package converter
 
 import (
 	"encoding/json"
-	"strings"
 )
 
 // CommaStringArray: Convert string array to comma separated string.
 // Example: CommaStringArray([]string{"a", "b", "c"}) => `["a","b","c"]`
 func CommaStringArray(data []string) string {
-	var result string
-	if len(data) > 0 {
-		result = "[\"" + strings.Join(data, "\",\"") + "\"]"
-	}
-	return result
+	jsonBytes, _ := json.Marshal(data)
+	return string(jsonBytes)
 }
 
 // StructToString: Convert struct to string.
