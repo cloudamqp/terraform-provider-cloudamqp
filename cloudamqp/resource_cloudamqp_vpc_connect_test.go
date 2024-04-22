8f4d87b0e8e3e77f04ba8f20ccd4efc60ebb914d
Tobias Brodén <tobias@84codes.com>
2024-04-22 19:38:29 +0200

    Update VPC Connect tests and cassettes

--- cloudamqp/resource_cloudamqp_vpc_connect_test.go
+++ cloudamqp/resource_cloudamqp_vpc_connect_test.go
@@ -13,17 +13,17 @@ import (
 // TestAccVpcConnect_AWS_Basic: Create standalone VPC and instance, enable VPC Connect and import.
 func TestAccVpcConnect_AWS_Basic(t *testing.T) {
 	var (
-		fileNames    = []string{"vpc_and_instance", "vpc_connect"}
-		vpcName      = "cloudamqp_vpc.vpc"
-		instanceName = "cloudamqp_instance.instance"
-		resourceName = "cloudamqp_vpc_connect.vpc_connect"
+		fileNames              = []string{"vpc_and_instance", "vpc_connect"}
+		vpcResourceName        = "cloudamqp_vpc.vpc"
+		instanceResourceName   = "cloudamqp_instance.instance"
+		vpcConnectResourceName = "cloudamqp_vpc_connect.vpc_connect"
 
 		params = map[string]string{
 			"VpcName":        "TestAccVpcConnect_AWS_Basic",
 			"VpcRegion":      "amazon-web-services::us-east-1",
 			"InstanceName":   "TestAccVpcConnect_AWS_Basic",
 			"InstanceRegion": "amazon-web-services::us-east-1",
-			"InstanceID":     fmt.Sprintf("%s.id", instanceName),
+			"InstanceID":     fmt.Sprintf("%s.id", instanceResourceName),
 			"VpcConnectAllowedPrincipals": converter.CommaStringArray([]string{
 				"arn:aws:iam::123456789012:root"}),
 		}
@@ -34,7 +34,7 @@ func TestAccVpcConnect_AWS_Basic(t *testing.T) {
 		//  "VpcRegion":      "amazon-web-services::us-east-1",
 		// 	"InstanceName":   "TestAccVpcConnect_AWS_Basic",
 		// 	"InstanceRegion": "amazon-web-services::us-east-1",
-		//  "InstanceID":    fmt.Sprintf("%s.id", instanceName),
+		//  "InstanceID":    fmt.Sprintf("%s.id", instanceResourceName),
 		// 	"VpcConnectAllowedPrincipals": converter.CommaStringArray([]string{
 		// 		"arn:aws:iam::123456789012:root",
 		// 		"arn:aws:iam::123456789012:user/username"}),
@@ -48,15 +48,15 @@ func TestAccVpcConnect_AWS_Basic(t *testing.T) {
 			{
 				Config: configuration.GetTemplatedConfig(t, fileNames, params),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(vpcName, "name", params["VpcName"]),
-					resource.TestCheckResourceAttr(instanceName, "name", params["InstanceName"]),
-					resource.TestCheckResourceAttr(resourceName, "allowed_principals.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "allowed_principals.0", "arn:aws:iam::123456789012:root"),
+					resource.TestCheckResourceAttr(vpcResourceName, "name", params["VpcName"]),
+					resource.TestCheckResourceAttr(instanceResourceName, "name", params["InstanceName"]),
+					resource.TestCheckResourceAttr(vpcConnectResourceName, "allowed_principals.#", "1"),
+					resource.TestCheckResourceAttr(vpcConnectResourceName, "allowed_principals.0", "arn:aws:iam::123456789012:root"),
 				),
 			},
 			{
-				ResourceName:            resourceName,
-				ImportStateIdFunc:       testAccImportStateIdFunc(resourceName),
+				ResourceName:            vpcConnectResourceName,
+				ImportStateIdFunc:       testAccImportStateIdFunc(vpcConnectResourceName),
 				ImportState:             true,
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"region", "approved_subscriptions", "allowed_projects"},
@@ -67,9 +67,9 @@ func TestAccVpcConnect_AWS_Basic(t *testing.T) {
 			// {
 			// 	Config: configuration.GetTemplatedConfig(t, fileNames, paramsUpdated),
 			// 	Check: resource.ComposeTestCheckFunc(
-			// 		resource.TestCheckResourceAttr(resourceName, "allowed_principals.#", "2"),
-			// 		resource.TestCheckResourceAttr(resourceName, "allowed_principals.0", "arn:aws:iam::123456789012:root"),
-			// 		resource.TestCheckResourceAttr(resourceName, "allowed_principals.1", "arn:aws:iam::123456789012:user/username"),
+			// 		resource.TestCheckResourceAttr(vpcConnectResourceName, "allowed_principals.#", "2"),
+			// 		resource.TestCheckResourceAttr(vpcConnectResourceName, "allowed_principals.0", "arn:aws:iam::123456789012:root"),
+			// 		resource.TestCheckResourceAttr(vpcConnectResourceName, "allowed_principals.1", "arn:aws:iam::123456789012:user/username"),
 			// 	),
 			// },
 		},
@@ -79,17 +79,17 @@ func TestAccVpcConnect_AWS_Basic(t *testing.T) {
 // TestAccVpcConnect_Azure_Basic: Create standalone VPC and instance, enable VPC Connect and import.
 func TestAccVpcConnect_Azure_Basic(t *testing.T) {
 	var (
-		fileNames    = []string{"vpc_and_instance", "vpc_connect"}
-		vpcName      = "cloudamqp_vpc.vpc"
-		instanceName = "cloudamqp_instance.instance"
-		resourceName = "cloudamqp_vpc_connect.vpc_connect"
+		fileNames              = []string{"vpc_and_instance", "vpc_connect"}
+		vpcResourceName        = "cloudamqp_vpc.vpc"
+		instanceResourceName   = "cloudamqp_instance.instance"
+		vpcConnectResourceName = "cloudamqp_vpc_connect.vpc_connect"
 
 		params = map[string]string{
 			"VpcName":        "TestAccVpcConnect_Azure_Basic",
 			"VpcRegion":      "azure-arm::eastus",
 			"InstanceName":   "TestAccVpcConnect_Azure_Basic",
 			"InstanceRegion": "azure-arm::eastus",
-			"InstanceID":     fmt.Sprintf("%s.id", instanceName),
+			"InstanceID":     fmt.Sprintf("%s.id", instanceResourceName),
 			"VpcConnectApprovedSubscriptions": converter.CommaStringArray([]string{
 				"56fab608-c846-4770-a493-e77f52c1ce41"}),
 		}
@@ -102,15 +102,15 @@ func TestAccVpcConnect_Azure_Basic(t *testing.T) {
 			{
 				Config: configuration.GetTemplatedConfig(t, fileNames, params),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(vpcName, "name", params["VpcName"]),
-					resource.TestCheckResourceAttr(instanceName, "name", params["InstanceName"]),
-					resource.TestCheckResourceAttr(resourceName, "approved_subscriptions.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "approved_subscriptions.0", "56fab608-c846-4770-a493-e77f52c1ce41"),
+					resource.TestCheckResourceAttr(vpcResourceName, "name", params["VpcName"]),
+					resource.TestCheckResourceAttr(instanceResourceName, "name", params["InstanceName"]),
+					resource.TestCheckResourceAttr(vpcConnectResourceName, "approved_subscriptions.#", "1"),
+					resource.TestCheckResourceAttr(vpcConnectResourceName, "approved_subscriptions.0", "56fab608-c846-4770-a493-e77f52c1ce41"),
 				),
 			},
 			{
-				ResourceName:            resourceName,
-				ImportStateIdFunc:       testAccImportStateIdFunc(resourceName),
+				ResourceName:            vpcConnectResourceName,
+				ImportStateIdFunc:       testAccImportStateIdFunc(vpcConnectResourceName),
 				ImportState:             true,
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"region", "allowed_principals", "allowed_projects"},
@@ -122,17 +122,17 @@ func TestAccVpcConnect_Azure_Basic(t *testing.T) {
 // TestAccVpcConnect_GCP_Basic: Create standalone VPC and instance, enable VPC Connect and import.
 func TestAccVpcConnect_GCP_Basic(t *testing.T) {
 	var (
-		fileNames    = []string{"vpc_and_instance", "vpc_connect"}
-		vpcName      = "cloudamqp_vpc.vpc"
-		instanceName = "cloudamqp_instance.instance"
-		resourceName = "cloudamqp_vpc_connect.vpc_connect"
+		fileNames              = []string{"vpc_and_instance", "vpc_connect"}
+		vpcResourceName        = "cloudamqp_vpc.vpc"
+		instanceResourceName   = "cloudamqp_instance.instance"
+		vpcConnectResourceName = "cloudamqp_vpc_connect.vpc_connect"
 
 		params = map[string]string{
 			"VpcName":        "TestAccVpcConnect_GCP_Basic",
 			"VpcRegion":      "google-compute-engine::us-west1",
 			"InstanceName":   "TestAccVpcConnect_GCP_Basic",
 			"InstanceRegion": "google-compute-engine::us-west1",
-			"InstanceID":     fmt.Sprintf("%s.id", instanceName),
+			"InstanceID":     fmt.Sprintf("%s.id", instanceResourceName),
 			"VpcConnectAllowedProjects": converter.CommaStringArray([]string{
 				"playground-84codes"}),
 		}
@@ -145,15 +145,15 @@ func TestAccVpcConnect_GCP_Basic(t *testing.T) {
 			{
 				Config: configuration.GetTemplatedConfig(t, fileNames, params),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(vpcName, "name", params["VpcName"]),
-					resource.TestCheckResourceAttr(instanceName, "name", params["InstanceName"]),
-					resource.TestCheckResourceAttr(resourceName, "allowed_projects.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "allowed_projects.0", "playground-84codes"),
+					resource.TestCheckResourceAttr(vpcResourceName, "name", params["VpcName"]),
+					resource.TestCheckResourceAttr(instanceResourceName, "name", params["InstanceName"]),
+					resource.TestCheckResourceAttr(vpcConnectResourceName, "allowed_projects.#", "1"),
+					resource.TestCheckResourceAttr(vpcConnectResourceName, "allowed_projects.0", "playground-84codes"),
 				),
 			},
 			{
-				ResourceName:            resourceName,
-				ImportStateIdFunc:       testAccImportStateIdFunc(resourceName),
+				ResourceName:            vpcConnectResourceName,
+				ImportStateIdFunc:       testAccImportStateIdFunc(vpcConnectResourceName),
 				ImportState:             true,
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"region", "allowed_principals", "approved_subscriptions"},
@@ -162,11 +162,11 @@ func TestAccVpcConnect_GCP_Basic(t *testing.T) {
 	})
 }
 
-func testAccImportStateIdFunc(resourceName string) resource.ImportStateIdFunc {
+func testAccImportStateIdFunc(vpcConnectResourceName string) resource.ImportStateIdFunc {
 	return func(state *terraform.State) (string, error) {
-		rs, ok := state.RootModule().Resources[resourceName]
+		rs, ok := state.RootModule().Resources[vpcConnectResourceName]
 		if !ok {
-			return "", fmt.Errorf("Resource %s not found", resourceName)
+			return "", fmt.Errorf("Resource %s not found", vpcConnectResourceName)
 		}
 		if rs.Primary.ID == "" {
 			return "", fmt.Errorf("No resource id set")
