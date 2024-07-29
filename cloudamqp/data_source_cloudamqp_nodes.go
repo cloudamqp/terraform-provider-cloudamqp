859fc412f4b5897d81e785e1f7dc33890c988270
Tobias Brodén <tobias@84codes.com>
2024-07-29 15:59:31 +0200

    Add internal hostname to nodes data source

--- cloudamqp/data_source_cloudamqp_nodes.go
+++ cloudamqp/data_source_cloudamqp_nodes.go
@@ -59,6 +59,10 @@ func dataSourceNodes() *schema.Resource {
 							Type:     schema.TypeInt,
 							Computed: true,
 						},
+						"internal_hostname": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
 					},
 				},
 			},
@@ -108,7 +112,8 @@ func validateNodesSchemaAttribute(key string) bool {
 		"configured",
 		"rmq_version",
 		"disk_size",
-		"additional_disk_size":
+		"additional_disk_size",
+		"internal_hostname":
 		return true
 	}
 	return false
