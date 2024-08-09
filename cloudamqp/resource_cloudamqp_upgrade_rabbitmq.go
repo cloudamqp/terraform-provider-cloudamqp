65e7f037b6eb623ad691d2f6356986e656b4245e
Tobias Brodén <tobias@84codes.com>
2024-08-09 09:40:36 +0200

    Extend how to upgrade RabbitMQ version
    Enable multiple ways to use the resource to upgrade RabbitMQ version.
    - Specify a available version
    - Bring to latest possible version with help of data source
    - Bring to latest possible version (old way)

--- cloudamqp/resource_cloudamqp_upgrade_rabbitmq.go
+++ cloudamqp/resource_cloudamqp_upgrade_rabbitmq.go
@@ -12,26 +12,44 @@ func resourceUpgradeRabbitMQ() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceUpgradeRabbitMQInvoke,
 		Read:   resourceUpgradeRabbitMQRead,
+		Update: resourceUpgradeRabbitMQUpdate,
 		Delete: resourceUpgradeRabbitMQRemove,
 		Schema: map[string]*schema.Schema{
 			"instance_id": {
 				Type:        schema.TypeInt,
-				ForceNew:    true,
 				Required:    true,
 				Description: "The CloudAMQP instance identifier",
+			},
+			"current_version": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Helper argument to change upgrade behaviour to latest possible version",
+			},
+			"new_version": {
+				Type:        schema.TypeString,
+				ForceNew:    true,
+				Optional:    true,
+				Description: "The new version to upgrade to",
 			},
 		},
 	}
 }
 
 func resourceUpgradeRabbitMQInvoke(d *schema.ResourceData, meta interface{}) error {
-	api := meta.(*api.API)
-	response, err := api.UpgradeRabbitMQ(d.Get("instance_id").(int))
+	var (
+		api             = meta.(*api.API)
+		instanceID      = d.Get("instance_id").(int)
+		current_version = d.Get("current_version").(string)
+		new_version     = d.Get("new_version").(string)
+	)
+
+	log.Printf("[DEBUG] - Upgrading RabbitMQ instance %d to version %s", instanceID, new_version)
+	response, err := api.UpgradeRabbitMQ(instanceID, current_version, new_version)
 	if err != nil {
 		return err
 	}
-	id := strconv.Itoa(d.Get("instance_id").(int))
-	d.SetId(id)
+
+	d.SetId(strconv.Itoa(instanceID))
 
 	if len(response) > 0 {
 		log.Printf("[INFO] - " + response)
@@ -41,6 +59,10 @@ func resourceUpgradeRabbitMQInvoke(d *schema.ResourceData, meta interface{}) err
 }
 
 func resourceUpgradeRabbitMQRead(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceUpgradeRabbitMQUpdate(d *schema.ResourceData, meta interface{}) error {
 	return nil
 }
 
