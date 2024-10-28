1ce1211e7affccea35abc4476b2b64fb653ac713
Tobias Brodén <47317658+tbroden84@users.noreply.github.com>
2024-10-28 17:03:44 +0100

    Fix: Incorrect schemas in plugin data sources (#300)
    ### WHY are these changes introduced?
    
    Reported issue: https://github.com/cloudamqp/terraform-provider-cloudamqp/issues/299 when using plugin data sources. Both data sources panics with: `interface conversion: interface {} is nil, not int`.
    
    Sleep and timeouts were added wrongly into respective schema: https://github.com/cloudamqp/terraform-provider-cloudamqp/pull/241
    
    ### WHAT is this pull request doing?
    
    - Moves sleep and timeout arguments to correct level in respective schema.
    - Updates the VCR plugins tests to contain the data sources.
    
    ### HOW can this pull request be tested?
    
    Run VCR tests.

--- cloudamqp/resource_cloudamqp_plugin_community_test.go
+++ cloudamqp/resource_cloudamqp_plugin_community_test.go
@@ -2,21 +2,23 @@ package cloudamqp
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/cloudamqp/terraform-provider-cloudamqp/cloudamqp/vcr-testing/configuration"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-// Playing the test benefits from having sleep set to default (10 s) while recording.
-// While using lower sleep (1 s) for replaying the test. This will speed up the test.
+// Recording the test benefits from having sleep set to default (10 s).
+// While replaying the test, using lower sleep (1 s), will speed up the test.
 
 // TestAccPluginCommunity_Basic: Install community plugin and check then disable it.
 func TestAccPluginCommunity_Basic(t *testing.T) {
 	var (
-		fileNames                   = []string{"instance", "plugin_community"}
-		instanceResourceName        = "cloudamqp_instance.instance"
-		communityPluginResourceName = "cloudamqp_plugin_community.rabbitmq_delayed_message_exchange"
+		fileNames                      = []string{"instance", "plugin_community", "data_source/plugins_community"}
+		instanceResourceName           = "cloudamqp_instance.instance"
+		communityPluginResourceName    = "cloudamqp_plugin_community.rabbitmq_delayed_message_exchange"
+		dataSourceCommunityPluginsName = "data.cloudamqp_plugins_community.community_plugins"
 
 		params = map[string]string{
 			"InstanceName":           "TestAccPluginCommunity_Basic",
@@ -47,6 +49,8 @@ func TestAccPluginCommunity_Basic(t *testing.T) {
 					resource.TestCheckResourceAttr(instanceResourceName, "name", params["InstanceName"]),
 					resource.TestCheckResourceAttr(communityPluginResourceName, "name", params["PluginCommunityName"]),
 					resource.TestCheckResourceAttr(communityPluginResourceName, "enabled", params["PluginCommunityEnabled"]),
+					resource.TestMatchResourceAttr(dataSourceCommunityPluginsName, "plugins.#", regexp.MustCompile(`[0-9]`)),
+					resource.TestCheckResourceAttr(dataSourceCommunityPluginsName, "timeout", "1800"),
 				),
 			},
 			{
@@ -54,6 +58,8 @@ func TestAccPluginCommunity_Basic(t *testing.T) {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(communityPluginResourceName, "name", paramsUpdated["PluginCommunityName"]),
 					resource.TestCheckResourceAttr(communityPluginResourceName, "enabled", paramsUpdated["PluginCommunityEnabled"]),
+					resource.TestMatchResourceAttr(dataSourceCommunityPluginsName, "plugins.#", regexp.MustCompile(`[0-9]`)),
+					resource.TestCheckResourceAttr(dataSourceCommunityPluginsName, "timeout", "1800"),
 				),
 			},
 		},
