1e8c2f2086527d2daa45db33ffce8a3ca30534c4
Tobias Brodén <tobias@84codes.com>
2025-03-13 07:59:22 +0100

    Add new resource

---
+++ cloudamqp/resource_cloudamqp_maintenance_window.go
@@ -0,0 +1,144 @@
+package cloudamqp
+
+import (
+	"fmt"
+	"regexp"
+	"slices"
+	"strconv"
+
+	"github.com/cloudamqp/terraform-provider-cloudamqp/api"
+	model "github.com/cloudamqp/terraform-provider-cloudamqp/api/models/instance"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceMaintenanceWindow() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceMaintenanceWindowUpdate,
+		Read:   resourceMaintenanceWindowRead,
+		Update: resourceMaintenanceWindowUpdate,
+		Delete: resourceMaintenanceWindowDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"instance_id": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Instance identifier",
+			},
+			"preferred_day": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Preferred day of the week when to run maintenance",
+				// ValidateDiagFunc: validatePreferredDay(),
+				ValidateFunc: func(value any, key string) (warns []string, errs []error) {
+					if value.(string) == "" {
+						return
+					}
+					days := []string{"Monday", "Tuesday", "Wednesday", "Thursday",
+						"Friday", "Saturday", "Sunday"}
+					if !slices.Contains(days, value.(string)) {
+						errs = append(errs, fmt.Errorf("should be a day of the week: %v", days))
+					}
+					return
+				},
+			},
+			"preferred_time": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Preferred time (UTC) the day when to run maintenance",
+				ValidateFunc: func(value any, key string) (warns []string, errs []error) {
+					if value.(string) == "" {
+						return
+					}
+					r, _ := regexp.Compile("^([0-1][0-9]|2[0-3]):([0-5][0-9])$")
+					if !r.MatchString(value.(string)) {
+						errs = append(errs, fmt.Errorf("the time: %s, in wrong on format hh:mm",
+							value.(string)))
+					}
+					return
+				},
+			},
+			"automatic_updates": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: "Enable automatic updates",
+				ValidateFunc: func(value any, key string) (warns []string, errs []error) {
+					if value.(string) == "" || value.(string) == "on" || value.(string) == "off" {
+						return
+					}
+					errs = append(errs, fmt.Errorf("valid values are [on, off]"))
+					return
+				},
+			},
+		},
+	}
+}
+
+func resourceMaintenanceWindowUpdate(d *schema.ResourceData, meta any) error {
+	var (
+		api        = meta.(*api.API)
+		instanceID = d.Get("instance_id").(int)
+		data       model.Maintenance
+	)
+
+	if v := d.Get("preferred_day"); v != nil {
+		data.PreferredDay = v.(string)
+	}
+
+	if v := d.Get("preferred_time"); v != nil {
+		data.PreferredTime = v.(string)
+	}
+
+	if v := d.Get("automatic_updates"); v != nil {
+		data.AutomaticUpdates = v.(string)
+	}
+
+	if err := api.UpdateMaintenance(instanceID, data); err != nil {
+		return err
+	}
+
+	d.SetId(strconv.Itoa(instanceID))
+	return nil
+}
+
+func resourceMaintenanceWindowRead(d *schema.ResourceData, meta any) error {
+	var (
+		api           = meta.(*api.API)
+		instanceID, _ = strconv.Atoi(d.Id())
+	)
+
+	// Set argument during import
+	if d.Get("instance_id").(int) == 0 {
+		d.Set("instance_id", instanceID)
+	}
+
+	data, err := api.ReadMaintenance(instanceID)
+	if err != nil {
+		return err
+	}
+
+	if data.PreferredDay == "None" {
+		d.Set("preferred_day", "")
+	} else {
+		d.Set("preferred_day", data.PreferredDay)
+	}
+
+	if data.PreferredTime == "None" {
+		d.Set("preferred_time", "")
+	} else {
+		d.Set("preferred_time", data.PreferredTime)
+	}
+
+	d.Set("automatic_updates", data.AutomaticUpdates)
+
+	return nil
+}
+
+func resourceMaintenanceWindowDelete(d *schema.ResourceData, meta any) error {
+	// Only remove from state
+	return nil
+}
