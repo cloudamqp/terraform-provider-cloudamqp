1e96e0da0789bed840779cb950dabfb982a0e9b4
Tobias Brodén <tobias@84codes.com>
2025-03-13 08:01:08 +0100

    Add test including new configurations and VCR fixtures

---
+++ cloudamqp/resource_cloudamqp_maintenance_window_test.go
@@ -0,0 +1,105 @@
+package cloudamqp
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cloudamqp/terraform-provider-cloudamqp/cloudamqp/vcr-testing/configuration"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+// TestAccMaintenanceWindow_Basic:
+func TestAccMaintenanceWindow_Basic(t *testing.T) {
+	var (
+		instanceResourceName          = "cloudamqp_instance.instance"
+		maintenanceWindowResourceName = "cloudamqp_maintenance_window.this"
+
+		name = "TestAccMaintenanceWindow_Basic"
+		plan = "penguin-1"
+
+		fileNamesDayTime = []string{"instance", "maintenance/set_day_and_time"}
+		paramsDayTime    = map[string]string{
+			"InstanceName":  name,
+			"InstanceID":    fmt.Sprintf("%s.id", instanceResourceName),
+			"InstancePlan":  plan,
+			"PreferredDay":  "Monday",
+			"PreferredTime": "01:00",
+		}
+
+		fileNamesDay = []string{"instance", "maintenance/set_only_day"}
+		paramsDay    = map[string]string{
+			"InstanceName": name,
+			"InstanceID":   fmt.Sprintf("%s.id", instanceResourceName),
+			"InstancePlan": plan,
+			"PreferredDay": "Tuesday",
+		}
+
+		fileNamesTime = []string{"instance", "maintenance/set_only_time"}
+		paramsTime    = map[string]string{
+			"InstanceName":  name,
+			"InstanceID":    fmt.Sprintf("%s.id", instanceResourceName),
+			"InstancePlan":  plan,
+			"PreferredTime": "02:00",
+		}
+
+		fileNamesAutomaticUpdates = []string{"instance", "maintenance/include_automatic_updates"}
+		paramsAutomaticUpdates    = map[string]string{
+			"InstanceName":     name,
+			"InstanceID":       fmt.Sprintf("%s.id", instanceResourceName),
+			"InstancePlan":     plan,
+			"PreferredDay":     "Monday",
+			"PreferredTime":    "01:00",
+			"AutomaticUpdates": "on",
+		}
+	)
+
+	cloudamqpResourceTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactory,
+		Steps: []resource.TestStep{
+			{
+				Config: configuration.GetTemplatedConfig(t, fileNamesDayTime, paramsDayTime),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(instanceResourceName, "name", paramsDayTime["InstanceName"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day",
+						paramsDayTime["PreferredDay"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time",
+						paramsDayTime["PreferredTime"]),
+				),
+			},
+			{
+				Config: configuration.GetTemplatedConfig(t, fileNamesDay, paramsDay),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(instanceResourceName, "name", paramsDay["InstanceName"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day",
+						paramsDay["PreferredDay"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time", ""),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "automatic_updates", "off"),
+				),
+			},
+			{
+				Config: configuration.GetTemplatedConfig(t, fileNamesTime, paramsTime),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(instanceResourceName, "name", paramsTime["InstanceName"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day", ""),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time",
+						paramsTime["PreferredTime"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "automatic_updates", "off"),
+				),
+			},
+			{
+				Config: configuration.GetTemplatedConfig(t, fileNamesAutomaticUpdates, paramsAutomaticUpdates),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(instanceResourceName, "name",
+						paramsAutomaticUpdates["InstanceName"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day",
+						paramsAutomaticUpdates["PreferredDay"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time",
+						paramsAutomaticUpdates["PreferredTime"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "automatic_updates",
+						paramsAutomaticUpdates["AutomaticUpdates"]),
+				),
+			},
+		},
+	})
+}
