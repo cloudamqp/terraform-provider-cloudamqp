0f197b203477e83c288ead451361285a83fd5a3d
Tobias Brodén <tobias@84codes.com>
2025-03-13 10:27:28 +0100

    Update test and VCR fixtures

--- cloudamqp/resource_cloudamqp_maintenance_window_test.go
+++ cloudamqp/resource_cloudamqp_maintenance_window_test.go
@@ -8,13 +8,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-// TestAccMaintenanceWindow_Basic:
-func TestAccMaintenanceWindow_Basic(t *testing.T) {
+func TestAccMaintenanceWindow_LavinMQ(t *testing.T) {
 	var (
 		instanceResourceName          = "cloudamqp_instance.instance"
 		maintenanceWindowResourceName = "cloudamqp_maintenance_window.this"
 
-		name = "TestAccMaintenanceWindow_Basic"
+		name = "TestAccMaintenanceWindow_LavinMQ"
 		plan = "penguin-1"
 
 		fileNamesDayTime = []string{"instance", "maintenance/set_day_and_time"}
@@ -98,6 +97,76 @@ func TestAccMaintenanceWindow_Basic(t *testing.T) {
 						paramsAutomaticUpdates["PreferredTime"]),
 					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "automatic_updates",
 						paramsAutomaticUpdates["AutomaticUpdates"]),
+				),
+			},
+		},
+	})
+}
+
+func TestAccMaintenanceWindow_RabbitMQ(t *testing.T) {
+	var (
+		instanceResourceName          = "cloudamqp_instance.instance"
+		maintenanceWindowResourceName = "cloudamqp_maintenance_window.this"
+
+		name = "TestAccMaintenanceWindow_RabbitMQ"
+		plan = "bunny-1"
+
+		fileNamesDayTime = []string{"instance", "maintenance/set_day_and_time"}
+		paramsDayTime    = map[string]string{
+			"InstanceName":  name,
+			"InstanceID":    fmt.Sprintf("%s.id", instanceResourceName),
+			"InstancePlan":  plan,
+			"PreferredDay":  "Monday",
+			"PreferredTime": "01:00",
+		}
+
+		fileNamesDay = []string{"instance", "maintenance/set_only_day"}
+		paramsDay    = map[string]string{
+			"InstanceName": name,
+			"InstanceID":   fmt.Sprintf("%s.id", instanceResourceName),
+			"InstancePlan": plan,
+			"PreferredDay": "Tuesday",
+		}
+
+		fileNamesTime = []string{"instance", "maintenance/set_only_time"}
+		paramsTime    = map[string]string{
+			"InstanceName":  name,
+			"InstanceID":    fmt.Sprintf("%s.id", instanceResourceName),
+			"InstancePlan":  plan,
+			"PreferredTime": "02:00",
+		}
+	)
+
+	cloudamqpResourceTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactory,
+		Steps: []resource.TestStep{
+			{
+				Config: configuration.GetTemplatedConfig(t, fileNamesDayTime, paramsDayTime),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(instanceResourceName, "name", paramsDayTime["InstanceName"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day",
+						paramsDayTime["PreferredDay"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time",
+						paramsDayTime["PreferredTime"]),
+				),
+			},
+			{
+				Config: configuration.GetTemplatedConfig(t, fileNamesDay, paramsDay),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(instanceResourceName, "name", paramsDay["InstanceName"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day",
+						paramsDay["PreferredDay"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time", ""),
+				),
+			},
+			{
+				Config: configuration.GetTemplatedConfig(t, fileNamesTime, paramsTime),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(instanceResourceName, "name", paramsTime["InstanceName"]),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day", ""),
+					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time",
+						paramsTime["PreferredTime"]),
 				),
 			},
 		},
