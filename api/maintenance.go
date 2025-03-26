4e551a3f3c18c684f1bd75b4612629891332a1af
Tobias Brodén <47317658+tbroden84@users.noreply.github.com>
2025-03-26 08:29:35 +0100

    Add support to managing maintenance window (#326)
    ### WHY are these changes introduced?
    
    Request to add a new resource to manage the maintenance window.
    
    Reference: https://github.com/cloudamqp/terraform-provider-cloudamqp/issues/322
    
    ### WHAT is this pull request doing?
    
    - Adds new client library endpoint with API JSON model.
    - Adds new resource `cloudamqp_maintenance_window`
    - Adds documentation for the new resource
    - Adds VCR configuration and fixtures
    - Use tflog instead of legacy logging

---
+++ api/maintenance.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"context"
+	"fmt"
+
+	model "github.com/cloudamqp/terraform-provider-cloudamqp/api/models/instance"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+func (api *API) SetMaintenance(ctx context.Context, instanceID int, data model.Maintenance) error {
+	var (
+		failed map[string]any
+		path   = fmt.Sprintf("/api/instances/%d/maintenance/settings", instanceID)
+	)
+
+	tflog.Debug(ctx, fmt.Sprintf("data: %v", data))
+
+	response, err := api.sling.New().Post(path).BodyJSON(data).Receive(nil, &failed)
+	if err != nil {
+		return err
+	}
+
+	switch response.StatusCode {
+	case 200:
+		return nil
+	default:
+		return fmt.Errorf("failed to update maintenance window, status: %d, message: %s",
+			response.StatusCode, failed)
+	}
+}
+
+func (api *API) ReadMaintenance(ctx context.Context, instanceID int) (model.Maintenance, error) {
+	var (
+		data   model.Maintenance
+		failed map[string]any
+		path   = fmt.Sprintf("/api/instances/%d/maintenance/settings", instanceID)
+	)
+
+	response, err := api.sling.New().Get(path).Receive(&data, &failed)
+	if err != nil {
+		return model.Maintenance{}, err
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("data: %v", data))
+
+	switch response.StatusCode {
+	case 200:
+		return data, nil
+	default:
+		return model.Maintenance{},
+			fmt.Errorf("read maintenance settings failed, status: %d, message: %s",
+				response.StatusCode, failed)
+	}
+}
---
+++ cloudamqp/resource_cloudamqp_maintenance_window.go
@@ -0,0 +1,156 @@
+package cloudamqp
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/cloudamqp/terraform-provider-cloudamqp/api"
+	model "github.com/cloudamqp/terraform-provider-cloudamqp/api/models/instance"
+	"github.com/cloudamqp/terraform-provider-cloudamqp/cloudamqp/utils"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceMaintenanceWindow() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceMaintenanceWindowUpdate,
+		ReadContext:   resourceMaintenanceWindowRead,
+		UpdateContext: resourceMaintenanceWindowUpdate,
+		DeleteContext: resourceMaintenanceWindowDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"instance_id": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Instance identifier",
+			},
+			"preferred_day": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Preferred day of the week when to run maintenance",
+				ValidateFunc: func(value any, key string) (warns []string, errs []error) {
+					if value.(string) == "" {
+						return
+					}
+					days := []string{"Monday", "Tuesday", "Wednesday", "Thursday",
+						"Friday", "Saturday", "Sunday"}
+					if !slices.Contains(days, value.(string)) {
+						errs = append(errs, fmt.Errorf("should be a day of the week: %v", days))
+					}
+					return
+				},
+			},
+			"preferred_time": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Preferred time (UTC) the day when to run maintenance",
+				ValidateFunc: func(value any, key string) (warns []string, errs []error) {
+					if value.(string) == "" {
+						return
+					}
+					r, _ := regexp.Compile("^([0-1][0-9]|2[0-3]):([0-5][0-9])$")
+					if !r.MatchString(value.(string)) {
+						errs = append(errs, fmt.Errorf("the time: %s, is in the wrong format hh:mm",
+							value.(string)))
+					}
+					return
+				},
+			},
+			"automatic_updates": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Computed:    true,
+				Description: "Enable automatic updates",
+				ValidateFunc: func(value any, key string) (warns []string, errs []error) {
+					val := strings.ToLower(value.(string))
+					if val == "" || val == "on" || val == "off" {
+						return
+					}
+					errs = append(errs, fmt.Errorf("valid values are [on, off]"))
+					return
+				},
+			},
+		},
+	}
+}
+
+func resourceMaintenanceWindowUpdate(ctx context.Context, d *schema.ResourceData,
+	meta any) diag.Diagnostics {
+
+	var (
+		api        = meta.(*api.API)
+		instanceID = d.Get("instance_id").(int)
+		data       model.Maintenance
+	)
+
+	if v := d.Get("preferred_day"); v != nil {
+		data.PreferredDay = v.(string)
+	}
+
+	if v := d.Get("preferred_time"); v != nil {
+		data.PreferredTime = v.(string)
+	}
+
+	if v := d.Get("automatic_updates"); v != nil {
+		if v.(string) == "on" {
+			data.AutomaticUpdates = utils.Pointer(true)
+		} else if v.(string) == "off" {
+			data.AutomaticUpdates = utils.Pointer(false)
+		}
+	}
+
+	if d.HasChanges("preferred_day", "preferred_time", "automatic_updates") {
+		if err := api.SetMaintenance(ctx, instanceID, data); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	d.SetId(strconv.Itoa(instanceID))
+	return diag.Diagnostics{}
+}
+
+func resourceMaintenanceWindowRead(ctx context.Context, d *schema.ResourceData,
+	meta any) diag.Diagnostics {
+
+	var (
+		api           = meta.(*api.API)
+		instanceID, _ = strconv.Atoi(d.Id())
+	)
+	// Set argument during import
+	if instanceIDValue, ok := d.Get("instance_id").(int); ok && instanceIDValue == 0 {
+		d.Set("instance_id", instanceID)
+	}
+
+	data, err := api.ReadMaintenance(ctx, instanceID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("preferred_day", data.PreferredDay)
+	d.Set("preferred_time", data.PreferredTime)
+
+	if data.AutomaticUpdates != nil {
+		if *data.AutomaticUpdates {
+			d.Set("automatic_updates", "on")
+		} else {
+			d.Set("automatic_updates", "off")
+		}
+	}
+
+	return diag.Diagnostics{}
+}
+
+func resourceMaintenanceWindowDelete(ctx context.Context, d *schema.ResourceData,
+	meta any) diag.Diagnostics {
+
+	// Only remove from state because the maintenance window is managed by the API
+	return diag.Diagnostics{}
+}
