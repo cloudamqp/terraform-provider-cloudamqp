bfa88d37f42279070fccb0bc1f292f6965259dfd
Anton Dalgren <anton@84codes.com>
2025-08-26 13:58:29 +0200

    refactor api path usage

--- api/oauth2_configuration.go
+++ api/oauth2_configuration.go
@@ -9,15 +9,17 @@ import (
 	job "github.com/cloudamqp/terraform-provider-cloudamqp/api/models/job"
 )
 
-func (api *API) ReadOAuth2Configuration(ctx context.Context, instanceID int, sleep time.Duration) (model.OAuth2ConfigResponse, error) {
-	path := fmt.Sprintf("/api/instances/%d/oauth2-configuration", instanceID)
+func getPath(instanceID int) string {
+	return fmt.Sprintf("/api/instances/%d/oauth2-configurations", instanceID)
+}
 
+func (api *API) ReadOAuth2Configuration(ctx context.Context, instanceID int, sleep time.Duration) (model.OAuth2ConfigResponse, error) {
 	var (
 		data   model.OAuth2ConfigResponse
 		failed map[string]any
 	)
 
-	err := callWithRetry(ctx, api.sling.New().Get(path), 1, sleep, &data, &failed)
+	err := callWithRetry(ctx, api.sling.New().Get(getPath(instanceID)), 1, sleep, &data, &failed)
 	if err != nil {
 		return model.OAuth2ConfigResponse{}, err
 	}
@@ -27,13 +29,12 @@ func (api *API) ReadOAuth2Configuration(ctx context.Context, instanceID int, sle
 
 func (api *API) CreateOAuth2Configuration(ctx context.Context, instanceID int, sleep time.Duration, params model.OAuth2ConfigRequest) (job.JobCreationResponse, error) {
 
-	path := fmt.Sprintf("/api/instances/%d/oauth2-configuration", instanceID)
 	var (
 		data   job.JobCreationResponse
 		failed map[string]any
 	)
 
-	err := callWithRetry(ctx, api.sling.New().Post(path).BodyJSON(&params), 1, sleep, &data, &failed)
+	err := callWithRetry(ctx, api.sling.New().Post(getPath(instanceID)).BodyJSON(&params), 1, sleep, &data, &failed)
 	if err != nil {
 		return job.JobCreationResponse{}, err
 	}
@@ -42,13 +43,12 @@ func (api *API) CreateOAuth2Configuration(ctx context.Context, instanceID int, s
 }
 
 func (api *API) UpdateOAuth2Configuration(ctx context.Context, instanceID int, sleep time.Duration, params model.OAuth2ConfigRequest) (job.JobCreationResponse, error) {
-	path := fmt.Sprintf("/api/instances/%d/oauth2-configuration", instanceID)
 	var (
 		data   job.JobCreationResponse
 		failed map[string]any
 	)
 
-	err := callWithRetry(ctx, api.sling.New().Put(path).BodyJSON(&params), 1, sleep, &data, &failed)
+	err := callWithRetry(ctx, api.sling.New().Put(getPath(instanceID)).BodyJSON(&params), 1, sleep, &data, &failed)
 	if err != nil {
 		return job.JobCreationResponse{}, err
 	}
@@ -57,13 +57,12 @@ func (api *API) UpdateOAuth2Configuration(ctx context.Context, instanceID int, s
 }
 
 func (api *API) DeleteOAuth2Configuration(ctx context.Context, instanceID int, sleep time.Duration) (job.JobCreationResponse, error) {
-	path := fmt.Sprintf("/api/instances/%d/oauth2-configuration", instanceID)
 	var (
 		data   job.JobCreationResponse
 		failed map[string]any
 	)
 
-	err := callWithRetry(ctx, api.sling.New().Delete(path), 1, sleep, &data, &failed)
+	err := callWithRetry(ctx, api.sling.New().Delete(getPath(instanceID)), 1, sleep, &data, &failed)
 	if err != nil {
 		return job.JobCreationResponse{}, err
 	}
