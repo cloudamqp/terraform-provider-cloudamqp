257e084c8b9973a00895826aaf018137aeb59b6e
Tobias Brodén <47317658+tbroden84@users.noreply.github.com>
2025-09-09 16:21:17 +0200

    Update handling of default values for Rabbitmq configuration resource (#372)
    ### WHY are these changes introduced?
    
    Issue with resource drift while setting default values from schema.
    
    ### WHAT is this pull request doing?
    
    - Adds generic retry for client library
    - Updates default values for sleep/timeout and set on import
    - Remove default value for arguments in schema and set on import
    - Restructure populating API response/request <-> resource model

--- cloudamqp/resource_cloudamqp_rabbitmq_configuration.go
+++ cloudamqp/resource_cloudamqp_rabbitmq_configuration.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/cloudamqp/terraform-provider-cloudamqp/api"
 	model "github.com/cloudamqp/terraform-provider-cloudamqp/api/models/instance/configuration"
@@ -21,7 +22,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 var (
@@ -93,7 +93,6 @@ func (r *rabbitMqConfigurationResource) Schema(ctx context.Context, req resource
 			"connection_max": schema.Int64Attribute{
 				Optional:    true,
 				Computed:    true,
-				Default:     int64default.StaticInt64(-1),
 				Description: "Set the maximum permissible number of connections, -1 means infinity.",
 				Validators: []validator.Int64{
 					int64validator.AtLeast(-1),
@@ -191,18 +190,12 @@ func (r *rabbitMqConfigurationResource) Schema(ctx context.Context, req resource
 				Default:     int64default.StaticInt64(60),
 				Computed:    true,
 				Description: "Configurable sleep time in seconds between retries for RabbitMQ configuration",
-				PlanModifiers: []planmodifier.Int64{
-					int64planmodifier.UseStateForUnknown(),
-				},
 			},
 			"timeout": schema.Int64Attribute{
 				Optional:    true,
 				Default:     int64default.StaticInt64(3600),
 				Computed:    true,
 				Description: "Configurable timeout time in seconds for RabbitMQ configuration",
-				PlanModifiers: []planmodifier.Int64{
-					int64planmodifier.UseStateForUnknown(),
-				},
 			},
 		},
 	}
@@ -231,54 +224,20 @@ func (r *rabbitMqConfigurationResource) Create(ctx context.Context, req resource
 	}
 
 	instanceID := int(plan.InstanceID.ValueInt64())
+	data := r.populateCreateRequest(&plan)
+
 	sleep := int(plan.Sleep.ValueInt64())
 	timeout := int(plan.Timeout.ValueInt64())
+	timeoutCtx, cancel := context.WithTimeout(ctx, time.Duration(timeout)*time.Second)
+	defer cancel()
 
-	data := model.RabbitMqConfigRequest{}
-	if !plan.Heartbeat.IsUnknown() {
-		data.Heartbeat = utils.Pointer(plan.Heartbeat.ValueInt64())
-	}
-	if !plan.ConnectionMax.IsUnknown() {
-		if plan.ConnectionMax.ValueInt64() == -1 {
-			data.ConnectionMax = &model.ConnectionMaxValue{IsInfinity: true}
-		} else {
-			data.ConnectionMax = &model.ConnectionMaxValue{IsInfinity: false, Value: plan.ConnectionMax.ValueInt64()}
-		}
-	}
-	if !plan.ChannelMax.IsUnknown() {
-		data.ChannelMax = utils.Pointer(plan.ChannelMax.ValueInt64())
-	}
-	if !plan.ConsumerTimeout.IsUnknown() {
-		if plan.ConsumerTimeout.ValueInt64() == -1 {
-			data.ConsumerTimeout = &model.ConsumerTimeoutValue{IsEnabled: false}
-		} else {
-			data.ConsumerTimeout = &model.ConsumerTimeoutValue{IsEnabled: true, Value: plan.ConsumerTimeout.ValueInt64()}
-		}
-	}
-	if !plan.VmMemoryHighWatermark.IsUnknown() {
-		data.VmMemoryHighWatermark = utils.Pointer(plan.VmMemoryHighWatermark.ValueFloat64())
-	}
-	if !plan.QueueIndexEmbedMsgsBelow.IsUnknown() {
-		data.QueueIndexEmbedMsgsBelow = utils.Pointer(plan.QueueIndexEmbedMsgsBelow.ValueInt64())
-	}
-	if !plan.MaxMessageSize.IsUnknown() {
-		data.MaxMessageSize = utils.Pointer(plan.MaxMessageSize.ValueInt64())
-	}
-	if !plan.LogExchangeLevel.IsUnknown() {
-		data.LogExchangeLevel = utils.Pointer(plan.LogExchangeLevel.ValueString())
-	}
-	if !plan.ClusterPartitionHandling.IsUnknown() {
-		data.ClusterPartitionHandling = utils.Pointer(plan.ClusterPartitionHandling.ValueString())
-	}
-
-	tflog.Debug(ctx, fmt.Sprintf("Creating RabbitMQ configuration for instance ID %d with data: %v", instanceID, data))
-	err := r.client.UpdateRabbitMqConfiguration(ctx, instanceID, data, sleep, timeout)
+	err := r.client.UpdateRabbitMqConfiguration(timeoutCtx, instanceID, data, sleep)
 	if err != nil {
 		resp.Diagnostics.AddError("API Error", fmt.Sprintf("Failed to create RabbitMQ configuration: %s", err.Error()))
 		return
 	}
 
-	dataResp, err := r.client.ReadRabbitMqConfiguration(ctx, instanceID, sleep, timeout)
+	dataResp, err := r.client.ReadRabbitMqConfiguration(ctx, instanceID, sleep)
 	if err != nil {
 		resp.Diagnostics.AddError("API Error", fmt.Sprintf("Failed to read RabbitMQ configuration: %s", err.Error()))
 		return
@@ -289,8 +248,7 @@ func (r *rabbitMqConfigurationResource) Create(ctx context.Context, req resource
 		return
 	}
 
-	tflog.Debug(ctx, fmt.Sprintf("Read RabbitMQ configuration data: %v", dataResp))
-	populateRabbitMqConfigModel(&plan, dataResp, instanceID, sleep, timeout)
+	r.populateRabbitMqConfigModel(&plan, dataResp, instanceID, sleep, timeout)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
 }
 
@@ -303,9 +261,16 @@ func (r *rabbitMqConfigurationResource) Read(ctx context.Context, req resource.R
 
 	instanceID := int(state.InstanceID.ValueInt64())
 	sleep := int(state.Sleep.ValueInt64())
-	timeout := int(state.Timeout.ValueInt64())
+	if sleep == 0 {
+		sleep = 60 // fallback default
+	}
 
-	data, err := r.client.ReadRabbitMqConfiguration(ctx, instanceID, sleep, timeout)
+	timeout := int(state.Timeout.ValueInt64())
+	if timeout == 0 {
+		timeout = 3600 // fallback default
+	}
+
+	data, err := r.client.ReadRabbitMqConfiguration(ctx, instanceID, sleep)
 	if err != nil {
 		resp.Diagnostics.AddError("API Error", err.Error())
 		return
@@ -317,15 +282,15 @@ func (r *rabbitMqConfigurationResource) Read(ctx context.Context, req resource.R
 		return
 	}
 
-	tflog.Info(ctx, fmt.Sprintf("Read RabbitMQ configuration data: %v", data))
-
-	populateRabbitMqConfigModel(&state, data, instanceID, sleep, timeout)
+	r.populateRabbitMqConfigModel(&state, data, instanceID, sleep, timeout)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
 }
 
 func (r *rabbitMqConfigurationResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
 	var plan rabbitMqConfigurationResourceModel
+	var state rabbitMqConfigurationResourceModel
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
@@ -333,51 +298,15 @@ func (r *rabbitMqConfigurationResource) Update(ctx context.Context, req resource
 	instanceID := int(plan.InstanceID.ValueInt64())
 	sleep := int(plan.Sleep.ValueInt64())
 	timeout := int(plan.Timeout.ValueInt64())
+	data := r.populateUpdateRequest(&plan)
 
-	data := model.RabbitMqConfigRequest{}
-	if !plan.Heartbeat.IsNull() {
-		data.Heartbeat = utils.Pointer(plan.Heartbeat.ValueInt64())
-	}
-	if !plan.ConnectionMax.IsNull() {
-		if plan.ConnectionMax.ValueInt64() == -1 {
-			data.ConnectionMax = &model.ConnectionMaxValue{IsInfinity: true}
-		} else {
-			data.ConnectionMax = &model.ConnectionMaxValue{IsInfinity: false, Value: plan.ConnectionMax.ValueInt64()}
-		}
-	}
-	if !plan.ChannelMax.IsNull() {
-		data.ChannelMax = utils.Pointer(plan.ChannelMax.ValueInt64())
-	}
-	if !plan.ConsumerTimeout.IsNull() {
-		if plan.ConsumerTimeout.ValueInt64() == -1 {
-			data.ConsumerTimeout = &model.ConsumerTimeoutValue{IsEnabled: false}
-		} else {
-			data.ConsumerTimeout = &model.ConsumerTimeoutValue{IsEnabled: true, Value: plan.ConsumerTimeout.ValueInt64()}
-		}
-	}
-	if !plan.VmMemoryHighWatermark.IsNull() {
-		data.VmMemoryHighWatermark = utils.Pointer(plan.VmMemoryHighWatermark.ValueFloat64())
-	}
-	if !plan.QueueIndexEmbedMsgsBelow.IsNull() {
-		data.QueueIndexEmbedMsgsBelow = utils.Pointer(plan.QueueIndexEmbedMsgsBelow.ValueInt64())
-	}
-	if !plan.MaxMessageSize.IsNull() {
-		data.MaxMessageSize = utils.Pointer(plan.MaxMessageSize.ValueInt64())
-	}
-	if !plan.LogExchangeLevel.IsNull() {
-		data.LogExchangeLevel = utils.Pointer(plan.LogExchangeLevel.ValueString())
-	}
-	if !plan.ClusterPartitionHandling.IsNull() {
-		data.ClusterPartitionHandling = utils.Pointer(plan.ClusterPartitionHandling.ValueString())
-	}
-
-	err := r.client.UpdateRabbitMqConfiguration(ctx, instanceID, data, sleep, timeout)
+	err := r.client.UpdateRabbitMqConfiguration(ctx, instanceID, data, sleep)
 	if err != nil {
 		resp.Diagnostics.AddError("API Error", fmt.Sprintf("Failed to update RabbitMQ configuration: %s", err.Error()))
 		return
 	}
 
-	dataResp, err := r.client.ReadRabbitMqConfiguration(ctx, instanceID, sleep, timeout)
+	dataResp, err := r.client.ReadRabbitMqConfiguration(ctx, instanceID, sleep)
 	if err != nil {
 		resp.Diagnostics.AddError("API Error", fmt.Sprintf("Failed to read RabbitMQ configuration: %s", err.Error()))
 		return
@@ -388,17 +317,17 @@ func (r *rabbitMqConfigurationResource) Update(ctx context.Context, req resource
 		return
 	}
 
-	tflog.Debug(ctx, fmt.Sprintf("Read RabbitMQ configuration data: %v", dataResp))
-	populateRabbitMqConfigModel(&plan, dataResp, instanceID, sleep, timeout)
+	r.populateRabbitMqConfigModel(&plan, dataResp, instanceID, sleep, timeout)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
 }
 
 func (r *rabbitMqConfigurationResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 	// No-op: configuration is not deleted from the server only removed from the state.
+	resp.State.RemoveResource(ctx)
 }
 
 func (r *rabbitMqConfigurationResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	id := req.ID // This is a string
+	id := req.ID
 	instanceID, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
 		resp.Diagnostics.AddError("Invalid import ID", fmt.Sprintf("Expected numeric instance_id, got: %q", id))
@@ -408,31 +337,140 @@ func (r *rabbitMqConfigurationResource) ImportState(ctx context.Context, req res
 	resp.State.SetAttribute(ctx, path.Root("instance_id"), instanceID)
 }
 
-// Helper to populate the resource model from the API response
-func populateRabbitMqConfigModel(model *rabbitMqConfigurationResourceModel, data *model.RabbitMqConfigResponse, instanceID, sleep, timeout int) {
-	model.ID = types.StringValue(strconv.Itoa(instanceID))
-	model.InstanceID = types.Int64Value(int64(instanceID))
-	model.Sleep = types.Int64Value(int64(sleep))
-	model.Timeout = types.Int64Value(int64(timeout))
-	model.Heartbeat = types.Int64Value(data.Heartbeat)
-	if data.ConnectionMax.IsInfinity {
-		model.ConnectionMax = types.Int64Value(-1)
+// Handle data conversion from API response to resource model
+func (r *rabbitMqConfigurationResource) populateRabbitMqConfigModel(resourceModel *rabbitMqConfigurationResourceModel,
+	data *model.RabbitMqConfigResponse, instanceID, sleep, timeout int) {
+
+	resourceModel.ID = types.StringValue(strconv.Itoa(instanceID))
+	resourceModel.InstanceID = types.Int64Value(int64(instanceID))
+	resourceModel.Sleep = types.Int64Value(int64(sleep))
+	resourceModel.Timeout = types.Int64Value(int64(timeout))
+	resourceModel.Heartbeat = types.Int64Value(data.Heartbeat)
+	resourceModel.ChannelMax = types.Int64Value(data.ChannelMax)
+	resourceModel.MaxMessageSize = types.Int64Value(data.MaxMessageSize)
+	resourceModel.LogExchangeLevel = types.StringValue(data.LogExchangeLevel)
+	resourceModel.ClusterPartitionHandling = types.StringValue(data.ClusterPartitionHandling)
+	resourceModel.VmMemoryHighWatermark = types.Float64Value(data.VmMemoryHighWatermark)
+
+	if data.ConnectionMax == nil {
+		resourceModel.ConnectionMax = types.Int64Value(-1) // Default value when not set
+	} else if data.ConnectionMax.IsInfinity {
+		resourceModel.ConnectionMax = types.Int64Value(-1)
 	} else {
-		model.ConnectionMax = types.Int64Value(data.ConnectionMax.Value)
+		resourceModel.ConnectionMax = types.Int64Value(data.ConnectionMax.Value)
 	}
-	model.ChannelMax = types.Int64Value(data.ChannelMax)
+
 	if !data.ConsumerTimeout.IsEnabled {
-		model.ConsumerTimeout = types.Int64Value(-1)
+		resourceModel.ConsumerTimeout = types.Int64Value(-1)
 	} else {
-		model.ConsumerTimeout = types.Int64Value(data.ConsumerTimeout.Value)
+		resourceModel.ConsumerTimeout = types.Int64Value(data.ConsumerTimeout.Value)
 	}
-	model.VmMemoryHighWatermark = types.Float64Value(data.VmMemoryHighWatermark)
+
 	if data.QueueIndexEmbedMsgsBelow == nil {
-		model.QueueIndexEmbedMsgsBelow = types.Int64Null()
+		resourceModel.QueueIndexEmbedMsgsBelow = types.Int64Value(4096) // Default value when not set
 	} else {
-		model.QueueIndexEmbedMsgsBelow = types.Int64Value(*data.QueueIndexEmbedMsgsBelow)
+		resourceModel.QueueIndexEmbedMsgsBelow = types.Int64Value(*data.QueueIndexEmbedMsgsBelow)
 	}
-	model.MaxMessageSize = types.Int64Value(data.MaxMessageSize)
-	model.LogExchangeLevel = types.StringValue(data.LogExchangeLevel)
-	model.ClusterPartitionHandling = types.StringValue(data.ClusterPartitionHandling)
+}
+
+// Handle data conversion from resource model to create API request
+func (r *rabbitMqConfigurationResource) populateCreateRequest(plan *rabbitMqConfigurationResourceModel) model.RabbitMqConfigRequest {
+	data := model.RabbitMqConfigRequest{}
+
+	if !plan.Heartbeat.IsUnknown() {
+		data.Heartbeat = utils.Pointer(plan.Heartbeat.ValueInt64())
+	}
+
+	if !plan.ConnectionMax.IsUnknown() {
+		if plan.ConnectionMax.ValueInt64() == -1 {
+			data.ConnectionMax = utils.Pointer(model.ConnectionMaxValue{IsInfinity: true})
+		} else {
+			data.ConnectionMax = utils.Pointer(model.ConnectionMaxValue{IsInfinity: false, Value: plan.ConnectionMax.ValueInt64()})
+		}
+	}
+
+	if !plan.ChannelMax.IsUnknown() {
+		data.ChannelMax = utils.Pointer(plan.ChannelMax.ValueInt64())
+	}
+
+	if !plan.ConsumerTimeout.IsUnknown() {
+		if plan.ConsumerTimeout.ValueInt64() == -1 {
+			data.ConsumerTimeout = utils.Pointer(model.ConsumerTimeoutValue{IsEnabled: false})
+		} else {
+			data.ConsumerTimeout = utils.Pointer(model.ConsumerTimeoutValue{IsEnabled: true, Value: plan.ConsumerTimeout.ValueInt64()})
+		}
+	}
+
+	if !plan.VmMemoryHighWatermark.IsUnknown() {
+		data.VmMemoryHighWatermark = utils.Pointer(plan.VmMemoryHighWatermark.ValueFloat64())
+	}
+
+	if !plan.QueueIndexEmbedMsgsBelow.IsUnknown() {
+		data.QueueIndexEmbedMsgsBelow = utils.Pointer(plan.QueueIndexEmbedMsgsBelow.ValueInt64())
+	}
+
+	if !plan.MaxMessageSize.IsUnknown() {
+		data.MaxMessageSize = utils.Pointer(plan.MaxMessageSize.ValueInt64())
+	}
+
+	if !plan.LogExchangeLevel.IsUnknown() {
+		data.LogExchangeLevel = plan.LogExchangeLevel.ValueString()
+	}
+
+	if !plan.ClusterPartitionHandling.IsUnknown() {
+		data.ClusterPartitionHandling = plan.ClusterPartitionHandling.ValueString()
+	}
+
+	return data
+}
+
+// Handle data conversion from resource model to update API request
+func (r *rabbitMqConfigurationResource) populateUpdateRequest(plan *rabbitMqConfigurationResourceModel) model.RabbitMqConfigRequest {
+	data := model.RabbitMqConfigRequest{}
+
+	if !plan.Heartbeat.IsNull() {
+		data.Heartbeat = utils.Pointer(plan.Heartbeat.ValueInt64())
+	}
+
+	if !plan.ConnectionMax.IsNull() {
+		if plan.ConnectionMax.ValueInt64() == -1 {
+			data.ConnectionMax = utils.Pointer(model.ConnectionMaxValue{IsInfinity: true})
+		} else {
+			data.ConnectionMax = utils.Pointer(model.ConnectionMaxValue{IsInfinity: false, Value: plan.ConnectionMax.ValueInt64()})
+		}
+	}
+
+	if !plan.ChannelMax.IsNull() {
+		data.ChannelMax = utils.Pointer(plan.ChannelMax.ValueInt64())
+	}
+
+	if !plan.ConsumerTimeout.IsNull() {
+		if plan.ConsumerTimeout.ValueInt64() == -1 {
+			data.ConsumerTimeout = utils.Pointer(model.ConsumerTimeoutValue{IsEnabled: false})
+		} else {
+			data.ConsumerTimeout = utils.Pointer(model.ConsumerTimeoutValue{IsEnabled: true, Value: plan.ConsumerTimeout.ValueInt64()})
+		}
+	}
+
+	if !plan.VmMemoryHighWatermark.IsNull() {
+		data.VmMemoryHighWatermark = utils.Pointer(plan.VmMemoryHighWatermark.ValueFloat64())
+	}
+
+	if !plan.QueueIndexEmbedMsgsBelow.IsNull() {
+		data.QueueIndexEmbedMsgsBelow = utils.Pointer(plan.QueueIndexEmbedMsgsBelow.ValueInt64())
+	}
+
+	if !plan.MaxMessageSize.IsNull() {
+		data.MaxMessageSize = utils.Pointer(plan.MaxMessageSize.ValueInt64())
+	}
+
+	if !plan.LogExchangeLevel.IsNull() {
+		data.LogExchangeLevel = plan.LogExchangeLevel.ValueString()
+	}
+
+	if !plan.ClusterPartitionHandling.IsNull() {
+		data.ClusterPartitionHandling = plan.ClusterPartitionHandling.ValueString()
+	}
+
+	return data
 }
