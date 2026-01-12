f71f9b732ed1b4d007a1018db879a47dd6374bad
Tobias Brodén <tobias@84codes.com>
2026-01-12 16:48:21 +0100

    VCR-test: Add test case for key-id to trigger replacement

--- cloudamqp/resource_cloudamqp_custom_certificate_test.go
+++ cloudamqp/resource_cloudamqp_custom_certificate_test.go
@@ -85,3 +85,77 @@ func TestAccCustomCertificate_Basic(t *testing.T) {
 		},
 	})
 }
+
+// TestAccCustomCertificate_KeyID: Creating dedicated AWS instance, upload custom certificate and
+// update with key identifier.
+func TestAccCustomCertificate_KeyID(t *testing.T) {
+	t.Parallel()
+
+	instanceResourceName := "cloudamqp_instance.instance"
+	customCertificateResourceName := "cloudamqp_custom_certificate.custom_cert"
+
+	// Set sanitized value for playback and use real value for recording
+	testCertificateCA := "TEST_CERTIFICATE_CA"
+	testCertificateCert := "TEST_CERTIFICATE_CERT"
+	testCertificatePrivateKey := "TEST_CERTIFICATE_PRIVATE_KEY"
+	if os.Getenv("CLOUDAMQP_RECORD") != "" {
+		testCertificateCA = os.Getenv("TEST_CERTIFICATE_CA")
+		testCertificateCert = os.Getenv("TEST_CERTIFICATE_CERT")
+		testCertificatePrivateKey = os.Getenv("TEST_CERTIFICATE_PRIVATE_KEY")
+	}
+
+	cloudamqpResourceTest(t, resource.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "cloudamqp_instance" "instance" {
+						name   = "TestAccCustomCertificate_KeyID"
+						plan   = "bunny-1"
+						region = "amazon-web-services::us-east-1"
+						tags   = []
+					}
+					
+					resource "cloudamqp_custom_certificate" "custom_cert" {
+						instance_id = cloudamqp_instance.instance.id
+						ca          = "%s"
+						cert        = "%s"
+						private_key = "%s"
+						sni_hosts   = "my.custom.domain"
+						key_id		  = "a918beb8-fee4-4de1-b0d5-873e2cb0eba2"
+					}
+				`, testCertificateCA, testCertificateCert, testCertificatePrivateKey),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(instanceResourceName, "name", "TestAccCustomCertificate_KeyID"),
+					resource.TestCheckResourceAttr(customCertificateResourceName, "sni_hosts", "my.custom.domain"),
+					resource.TestCheckResourceAttr(customCertificateResourceName, "key_id", "a918beb8-fee4-4de1-b0d5-873e2cb0eba2"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+					resource "cloudamqp_instance" "instance" {
+						name   = "TestAccCustomCertificate_KeyID"
+						plan   = "bunny-1"
+						region = "amazon-web-services::us-east-1"
+						tags   = []
+					}
+					
+					# Increment version to trigger force new, restore default and re-upload custom certificate.
+					resource "cloudamqp_custom_certificate" "custom_cert" {
+						instance_id = cloudamqp_instance.instance.id
+						ca          = "%s"
+						cert        = "%s"
+						private_key = "%s"
+						sni_hosts   = "my.custom.domain"
+						key_id		  = "53f188e8-a81d-4232-b5f1-7379b0223bb1"
+					}
+				`, testCertificateCA, testCertificateCert, testCertificatePrivateKey),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(instanceResourceName, "name", "TestAccCustomCertificate_KeyID"),
+					resource.TestCheckResourceAttr(customCertificateResourceName, "sni_hosts", "my.custom.domain"),
+					resource.TestCheckResourceAttr(customCertificateResourceName, "key_id", "53f188e8-a81d-4232-b5f1-7379b0223bb1"),
+				),
+			},
+		},
+	})
+}
