defbd17cac628fdf07d77c70bc66e9167f447bd7
Tobias Brodén <tobias@84codes.com>
2026-04-15 17:38:58 +0300

    VCR-test: Update tests and re-record cassettes

--- cloudamqp/resource_cloudamqp_maintenance_window_test.go
+++ cloudamqp/resource_cloudamqp_maintenance_window_test.go
@@ -11,93 +11,86 @@ import (
 func TestAccMaintenanceWindow_LavinMQ(t *testing.T) {
 	t.Parallel()
 
-	var (
-		instanceResourceName          = "cloudamqp_instance.instance"
-		maintenanceWindowResourceName = "cloudamqp_maintenance_window.this"
-
-		name = "TestAccMaintenanceWindow_LavinMQ"
-		plan = "penguin-1"
-
-		fileNamesDayTime = []string{"instance", "maintenance/set_day_and_time"}
-		paramsDayTime    = map[string]string{
-			"InstanceName":  name,
-			"InstanceID":    fmt.Sprintf("%s.id", instanceResourceName),
-			"InstancePlan":  plan,
-			"PreferredDay":  "Monday",
-			"PreferredTime": "01:00",
-		}
-
-		fileNamesDay = []string{"instance", "maintenance/set_only_day"}
-		paramsDay    = map[string]string{
-			"InstanceName": name,
-			"InstanceID":   fmt.Sprintf("%s.id", instanceResourceName),
-			"InstancePlan": plan,
-			"PreferredDay": "Tuesday",
-		}
-
-		fileNamesTime = []string{"instance", "maintenance/set_only_time"}
-		paramsTime    = map[string]string{
-			"InstanceName":  name,
-			"InstanceID":    fmt.Sprintf("%s.id", instanceResourceName),
-			"InstancePlan":  plan,
-			"PreferredTime": "02:00",
-		}
-
-		fileNamesAutomaticUpdates = []string{"instance", "maintenance/set_maintenance"}
-		paramsAutomaticUpdates    = map[string]string{
-			"InstanceName":     name,
-			"InstanceID":       fmt.Sprintf("%s.id", instanceResourceName),
-			"InstancePlan":     plan,
-			"PreferredDay":     "Monday",
-			"PreferredTime":    "01:00",
-			"AutomaticUpdates": "on",
-		}
-	)
-
 	cloudamqpResourceTest(t, resource.TestCase{
 		PreCheck: func() { testAccPreCheck(t) },
 		Steps: []resource.TestStep{
 			{
-				Config: configuration.GetTemplatedConfig(t, fileNamesDayTime, paramsDayTime),
+				Config: `
+          resource "cloudamqp_instance" "instance" {
+            name   = "TestAccMaintenanceWindow_LavinMQ"
+            region = "amazon-web-services::us-east-1"
+            plan   = "penguin-1"
+            tags   = ["vcr-test"]
+				  }
+						
+          resource "cloudamqp_maintenance_window" "this" {
+            instance_id    = cloudamqp_instance.instance.id
+            preferred_day  = "Monday"
+            preferred_time = "01:00"
+          }`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(instanceResourceName, "name", paramsDayTime["InstanceName"]),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day",
-						paramsDayTime["PreferredDay"]),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time",
-						paramsDayTime["PreferredTime"]),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "preferred_day", "Monday"),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "preferred_time", "01:00"),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "automatic_updates", "on"),
 				),
 			},
 			{
-				Config: configuration.GetTemplatedConfig(t, fileNamesDay, paramsDay),
+				Config: `
+          resource "cloudamqp_instance" "instance" {
+            name   = "TestAccMaintenanceWindow_LavinMQ"
+            region = "amazon-web-services::us-east-1"
+            plan   = "penguin-1"
+            tags   = ["vcr-test"]
+				  }
+						
+          resource "cloudamqp_maintenance_window" "this" {
+            instance_id   = cloudamqp_instance.instance.id
+            preferred_day = "Tuesday"
+          }`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(instanceResourceName, "name", paramsDay["InstanceName"]),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day",
-						paramsDay["PreferredDay"]),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time", ""),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "automatic_updates", "off"),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "preferred_day", "Tuesday"),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "preferred_time", ""),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "automatic_updates", "on"),
 				),
 			},
 			{
-				Config: configuration.GetTemplatedConfig(t, fileNamesTime, paramsTime),
+				Config: `
+          resource "cloudamqp_instance" "instance" {
+            name   = "TestAccMaintenanceWindow_LavinMQ"
+            region = "amazon-web-services::us-east-1"
+            plan   = "penguin-1"
+            tags   = ["vcr-test"]
+          }
+	
+          resource "cloudamqp_maintenance_window" "this" {
+            instance_id    = cloudamqp_instance.instance.id
+            preferred_time = "02:00"
+          }`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(instanceResourceName, "name", paramsTime["InstanceName"]),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day", ""),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time",
-						paramsTime["PreferredTime"]),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "automatic_updates", "off"),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "preferred_day", ""),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "preferred_time", "02:00"),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "automatic_updates", "on"),
 				),
 			},
 			{
-				Config: configuration.GetTemplatedConfig(t, fileNamesAutomaticUpdates, paramsAutomaticUpdates),
+				Config: `
+          resource "cloudamqp_instance" "instance" {
+            name   = "TestAccMaintenanceWindow_LavinMQ"
+            region = "amazon-web-services::us-east-1"
+            plan   = "penguin-1"
+            tags   = ["vcr-test"]
+				  }
+						
+          resource "cloudamqp_maintenance_window" "this" {
+            instance_id       = cloudamqp_instance.instance.id
+            preferred_day     = "Monday"
+            preferred_time    = "01:00"
+            automatic_updates = "off"
+          }`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(instanceResourceName, "name",
-						paramsAutomaticUpdates["InstanceName"]),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_day",
-						paramsAutomaticUpdates["PreferredDay"]),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "preferred_time",
-						paramsAutomaticUpdates["PreferredTime"]),
-					resource.TestCheckResourceAttr(maintenanceWindowResourceName, "automatic_updates",
-						paramsAutomaticUpdates["AutomaticUpdates"]),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "preferred_day", "Monday"),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "preferred_time", "01:00"),
+					resource.TestCheckResourceAttr("cloudamqp_maintenance_window.this", "automatic_updates", "off"),
 				),
 			},
 		},
