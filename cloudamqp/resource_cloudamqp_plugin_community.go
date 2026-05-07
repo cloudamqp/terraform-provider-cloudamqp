0d681d74fc8c89e00338698e98017842ab217095
Tobias Brodén <tobias@84codes.com>
2026-05-07 12:20:55 +0300

    Use correct endpoint to fetch plugin information

--- cloudamqp/resource_cloudamqp_plugin_community.go
+++ cloudamqp/resource_cloudamqp_plugin_community.go
@@ -129,10 +129,10 @@ func resourcePluginCommunityRead(ctx context.Context, d *schema.ResourceData, me
 		return diag.Errorf("missing instance identifier: {resource_id},{instance_id}")
 	}
 
-	data, err := api.ReadPluginCommunity(ctx, instanceID, name, sleep, timeout)
 	timeoutCtx, cancel := context.WithTimeout(ctx, time.Duration(timeout)*time.Second)
 	defer cancel()
 
+	data, err := api.ReadPlugin(timeoutCtx, instanceID, name, sleep, timeout)
 	if err != nil {
 		// If instance not found (404), return nil to indicate resource not found
 		// This allows Terraform to recreate the resource when the instance is recreated
